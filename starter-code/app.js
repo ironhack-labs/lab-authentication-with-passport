71dafecfc593577b336c3effc345b1511615c9a8
Jorge Abad <jorgejoseabad@gmail.com>
2017-02-13 16:35:31 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -21,19 +21,17 @@ const LocalStrategy = require("passport-local").Strategy;
 const flash = require("connect-flash");
 
 
-
-
-
 //enable sessions here
-
-
-
+app.use(session({
+  secret: "passport-local-strategy",
+  resave: true,
+  saveUninitialized: true
+}));
 
 //initialize passport and session here
-
-
-
-
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash()); //para la gestion del error en caso de incorrect username o password
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -45,22 +43,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 // require in the routers
 app.use('/', index);
 app.use('/', users);
 app.use('/', passportRouter);
 
-
-
-
-
 //passport code here
 
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
 
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
 
-
-
+    return next(null, user);
+  });
+}));
 
 
 
