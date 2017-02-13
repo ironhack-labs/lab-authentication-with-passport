b3d454b825c1c3829bf8405e058231ceea0b197e
Amanda Honkanen <amanda.honkanen@me.com>
2017-02-13 15:24:43 +0100

    finished pair programming

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,7 +11,7 @@ var users = require('./routes/users');
 const passportRouter = require("./routes/passportRouter");
 //mongoose configuration
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/passport-local");
+mongoose.connect("mongodb://localhost:27017/passport-local");
 //require the user model
 const User = require("./models/user");
 const session       = require("express-session");
@@ -24,13 +24,21 @@ const flash = require("connect-flash");
 
 
 
+
 //enable sessions here
+app.use(session({
+  secret: "passport-local-strategy",
+  resave: true,
+  saveUninitialized: true
+}));
 
 
 
 
 //initialize passport and session here
-
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
@@ -55,13 +63,32 @@ app.use('/', passportRouter);
 
 
 //passport code here
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
 
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
 
-
-
-
-
+    return next(null, user);
+  });
+}));
 
 
 
