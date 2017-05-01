a39e5d2712a49a14f586bad9c55a4bf5a77140ae
edsonjmv <edsonjmv@gmail.com>
2017-05-01 14:49:50 +0200

    done

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6*/
 const express        = require("express");
 const router         = express.Router();
 // User model
@@ -8,14 +9,57 @@ const bcryptSalt     = 10;
 const ensureLogin = require("connect-ensure-login");
 const passport      = require("passport");
 
-
-
-router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+router.get("/private", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
+router.get('/signup', (req, res) => {
+  res.render('passport/signup');
+});
 
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
+  if (username === "" || password === "") {
+    res.render("/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt     = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = User({
+      username: username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/login");
+      }
+    });
+  });
+});
+
+router.get('/login', (req, res) => {
+  res.render('passport/login');
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/private",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
 
 
 
