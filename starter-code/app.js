8e189c219d0df36f90e1fc480849acadf94788d5
Hery Martin <scorpionxii@gmail.com>
2017-05-01 15:40:13 +0200

    First Commit!

--- starter-code/app.js
+++ starter-code/app.js
@@ -15,23 +15,51 @@ mongoose.connect("mongodb://localhost/passport-local");
 //require the user model
 const User = require("./models/user");
 const session       = require("express-session");
-const bcrypt        = require("bcrypt");
+const bcrypt        = require("bcryptjs");
 const passport      = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const flash = require("connect-flash");
 
-
-
-
-
 //enable sessions here
+app.use(session({
+    secret: "our-passport-local-strategy-app",
+    resave: true,
+    saveUninitialized: true
+}));
 
+passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+    User.findOne({ "_id": id }, (err, user) => {
+        if (err) { return cb(err); }
+        cb(null, user);
+    });
+});
+
+app.use(flash());
+passport.use(new LocalStrategy((username, password, next) => {
+    User.findOne({ username }, (err, user) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return next(null, false, { message: "Incorrect username" });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+            return next(null, false, { message: "Incorrect password" });
+        }
+
+        return next(null, user);
+    });
+}));
 
 
 
 //initialize passport and session here
-
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
@@ -50,19 +78,7 @@ app.use('/', index);
 app.use('/', users);
 app.use('/', passportRouter);
 
-
-
-
-
 //passport code here
-
-
-
-
-
-
-
-
 
 
 // catch 404 and forward to error handler
