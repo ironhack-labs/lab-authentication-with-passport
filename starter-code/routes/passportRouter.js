1606188e3e27dda68330f7de7ef209bbfe35a3e6
Xenock <miguelmoracho@gmail.com>
2017-05-01 14:40:55 +0200

    added auth feature. iteration1 finished

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,22 +1,49 @@
-const express        = require("express");
-const router         = express.Router();
+// routes/auth-routes.js
+const express    = require("express")
+const authRoutes = express.Router()
+
 // User model
-const User           = require("../models/user");
+const User       = require("../models/user")
+
 // Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
-const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+const bcrypt     = require("bcrypt")
+const bcryptSalt = 10
 
+authRoutes.get("/signup", (req, res, next) => {
+  res.render("passport/signup")
+})
 
+authRoutes.post("/signup", (req, res, next) => {
+  const username = req.body.username
+  const password = req.body.password
 
-router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
-});
+  if (username === "" || password === "") {
+    res.render("passport/signup", { message: "Indicate username and password" })
+    return
+  }
 
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("passport/signup", { message: "The username already exists" })
+      return
+    }
 
+    const salt     = bcrypt.genSaltSync(bcryptSalt)
+    const hashPass = bcrypt.hashSync(password, salt)
 
+    const newUser = User({
+      username: username,
+      password: hashPass
+    })
 
+    newUser.save((err) => {
+      if (err) {
+        res.render("passport/signup", { message: "Something went wrong" })
+      } else {
+        res.redirect("/")
+      }
+    })
+  })
+})
 
-
-module.exports = router;
+module.exports = authRoutes
