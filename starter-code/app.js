5b95bc74e3c8a8ee16dba63e892a0b31898af944
Cyril Alvarez <cyrilalavrezadriaansen@gmail.com>
2017-05-02 17:15:05 +0200

    push

--- starter-code/app.js
+++ starter-code/app.js
@@ -21,18 +21,46 @@ const LocalStrategy = require("passport-local").Strategy;
 const flash = require("connect-flash");
 
 
-
-
-
 //enable sessions here
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
 
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
 
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
+app.use(flash());
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
 
 //initialize passport and session here
-
-
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // view engine setup
@@ -49,21 +77,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/', users);
 app.use('/', passportRouter);
-
-
-
-
-
-//passport code here
-
-
-
-
-
-
-
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
