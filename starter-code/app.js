31da3296ff9b0f95309f9da6b976231491dfc858
Alice Guan <aliguan726@gmail.com>
2017-05-06 16:48:10 -0400

    Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,7 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const layouts = require('express-ejs-layouts');
 var app = express();
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,18 +23,16 @@ const LocalStrategy = require("passport-local").Strategy;
 const flash = require("connect-flash");
 
 
-
-
-
 //enable sessions here
-
-
-
+app.use( session ({
+    secret: 'random signup app',
+    resave: true,
+    saveUninitialized: true,
+}) );
 
 //initialize passport and session here
-
-
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // view engine setup
@@ -45,23 +45,70 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(layouts);
 // require in the routers
 app.use('/', index);
 app.use('/', users);
 app.use('/', passportRouter);
 
 
-
-
-
 //passport code here
+app.use((req, res, next) => {
+    if(req.user) {
+        res.locals.user = req.user;
+    }
+    next();
+});
 
+//determines what to save in the session
+//called when you log in
+passport.serializeUser((user, cb) => {
+    cb(null, user._id);
+});
 
+//called on every request after you login
+passport.deserializeUser((userId, cb) => {
+    //query the database for ID
+    User.findById( userId, (err, theUser) => {
+        if(err) {
+            cb(err);
+            return;
+        }
+        // sending the user's info to passport
+        cb(null, theUser);
+    });
+});
 
+passport.use( new LocalStrategy (
+    {
+        usernameField: 'loginUsername',
+        passwordField: 'loginPassword'
+    },
+    //2nd argument validates the login
+    ( loginUsername, loginPassword, next ) => {
+        User.findOne(
+            {
+                username: loginUsername,
+            },
+            (err, theUser) => {
+                if(err) {
+                    next(err);
+                    return;
+                }
+                if(!theUser) {
+                    next(null, false);
+                    return;
+                }
+                if(!bcrypt.compareSync(loginPassword, theUser.password)) {
+                    next(null, false);
+                    return;
+                }
 
-
-
-
+                next(null, theUser);
+            });
+    }
+)
+);
 
 
 
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -5,18 +5,84 @@ const User           = require("../models/user");
 // Bcrypt to encrypt passwords
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
-const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
-
+const ensureLogin    = require("connect-ensure-login");
+const passport       = require("passport");
+const mongoose       = require('mongoose');
+const layouts        = require('express-ejs-layouts');
 
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+  res.render("passport/private.ejs", { user: req.user });
+});
+
+
+router.get('/signup', (req, res, next) => {
+    res.render('passport/signup.ejs');
+});
+
+router.post('/signup', (req, res, next) => {
+    const signupUsername = req.body.signupUsername;
+    const signupPassword = req.body.signupPassword;
+
+    if( !signupUsername || !signupPassword ) {
+        res.render('passport/signup',
+            {
+                errorMessage: 'Please enter username and password'
+            }
+        );
+        return;
+    }
+
+    User.findOne(
+        { username: signupUsername },
+        { username: 1 },
+        //callback
+        (err, foundUser) => {
+            if(err) {
+                next(err);
+                return;
+            }
+
+            if(foundUser) {
+                res.render('/signup', {
+                    errorMessage: 'This username is already taken'
+                });
+                return;
+            }
+
+            const salt = bcrypt.genSaltSync(10);
+            //encrypt password with bcrypt
+            const encryptedPass = bcrypt.hashSync(signupPassword, salt);
+
+            //create new user based on information from form
+            const newUser = new User({
+                username: signupUsername,
+                password: encryptedPass
+            });
+            //Save the newly created user
+            newUser.save((err) => {
+                if(err) {
+                    next(err);
+                    return;
+                }
+                res.redirect('/');
+            });
+        }
+    );
+
+});
+
+router.get('/login', (req, res, next) => {
+    res.render('passport/login');
 });
 
 
 
-
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    })
+);
 
 
 module.exports = router;
