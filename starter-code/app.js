e4238c97d98fde552ccd4f0b8e9e3642040bfaf4
artseven <yabkevich.artsiom.09@gmail.com>
2017-05-09 17:49:17 -0400

    Not finished

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,25 +1,25 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var app = express();
+var express          = require('express');
+var path             = require('path');
+var favicon          = require('serve-favicon');
+var logger           = require('morgan');
+var cookieParser     = require('cookie-parser');
+var bodyParser       = require('body-parser');
+var app              = express();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index            = require('./routes/index');
+var users            = require('./routes/users');
 const passportRouter = require("./routes/passportRouter");
 //mongoose configuration
-const mongoose = require("mongoose");
+const mongoose       = require("mongoose");
 mongoose.connect("mongodb://localhost/passport-local");
 //require the user model
-const User = require("./models/user");
-const session       = require("express-session");
-const bcrypt        = require("bcrypt");
-const passport      = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const flash = require("connect-flash");
-
+const User           = require("./models/user");
+const session        = require("express-session");
+const bcrypt         = require("bcrypt");
+const passport       = require("passport");
+const LocalStrategy  = require("passport-local").Strategy;
+const flash          = require("connect-flash");
+const ensureLogin    = require("connect-ensure-login");
 
 
 
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -5,14 +5,54 @@ const User           = require("../models/user");
 // Bcrypt to encrypt passwords
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
-const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+const ensureLogin    = require("connect-ensure-login");
+const passport       = require("passport");
 
 
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+router.get('/signup',
+  ensureLogin.ensureNotLoggedIn('/'),
+  (req, res, next) => {
+  res.render('passport/signup.ejs');
+});
+
+router.post('/signup',
+  ensureLogin.ensureNotLoggedIn('/')),
+  (req, res, next) => {
+    const signupUsername = req.body.signupUsername;
+    const signupPassword = req.body.signupPassword;
+
+  // Don't let users submit blank usernames or passwords
+    if (signupUsername === '' || signupPassword === '') {
+      res.render('passport/signup.ejs', {
+        errorMessage: 'Please provide both username and password'
+      });
+      return;
+    }
+    User.findOne(
+      { username: signupUsername},
+      { username: 1},
+      (err, foundUser) => {
+        if (err) {
+          next(err);
+          return;
+        }
+        if (foundUser) {
+          res.render('passport/signup,ejs', {
+            errorMessage: 'Username already exists'
+          });
+          return;
+        }
+
+      }
+    )
+
+  }
+
 
 
 
