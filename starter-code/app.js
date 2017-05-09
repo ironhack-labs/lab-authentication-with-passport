d530aebeb0efb884ef8297e56f1400e041f1e0e4
sandrabosk <Sandra@Aleksandras-MBP.att.net>
2017-05-09 00:34:56 -0400

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,39 +1,84 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var app = express();
+var express       = require('express');
+var path          = require('path');
+var favicon       = require('serve-favicon');
+var logger        = require('morgan');
+const layouts     = require('express-ejs-layouts');
+var cookieParser  = require('cookie-parser');
+var bodyParser    = require('body-parser');
+var app           = express();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-const passportRouter = require("./routes/passportRouter");
+var index           = require('./routes/index');
+var users           = require('./routes/users');
+const passportRouter= require("./routes/passportRouter");
 //mongoose configuration
-const mongoose = require("mongoose");
+const mongoose      = require("mongoose");
 mongoose.connect("mongodb://localhost/passport-local");
 //require the user model
-const User = require("./models/user");
+const User          = require("./models/user");
 const session       = require("express-session");
 const bcrypt        = require("bcrypt");
 const passport      = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const flash = require("connect-flash");
-
-
-
-
+const flash         = require("connect-flash");
 
 //enable sessions here
+app.use(session({
+  secret:'my app',
+  resave: true,
+  saveUninitialized: true
+ }) );
 
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use ((req, res, next)=>{
+  if(req.user){
+    res.locals.user = req.user;
+  }
+  next();
+});
 
+//passport code here
 
-//initialize passport and session here
+passport.serializeUser((user, cb)=>{
+  cb ( null, user._id );
+});
 
+passport.deserializeUser((userId, cb) => {
+  User.findById(userId, (err, theUser) => {
+    if (err){
+      cb(err);
+      return;
+    }
+    cb(null, theUser);
+  });
+});
+passport.use(new LocalStrategy(
+    {
+      usernameField: 'loginUsername',
+      passwordField: 'loginPassword'
+    },
+  ( loginUsername, loginPassword, next )=>{
+    User.findOne({ username: loginUsername },
+      (err, theUser)=>{
+        if (err){
+          next(err);
+          return;
+        }
+        if (!theUser){
+          next(null, false);
+          return;
+        }
 
-
-
+        if (!bcrypt.compareSync(loginPassword, theUser.encryptedPassword)){
+          next(null, false);
+          return;
+        }
+        next(null, theUser);
+      }
+    );
+  }
+));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -45,25 +90,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(layouts);
+
 // require in the routers
 app.use('/', index);
 app.use('/', users);
 app.use('/', passportRouter);
-
-
-
-
-
-//passport code here
-
-
-
-
-
-
-
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
