56ea7b228840536fe62f931d67a6593ec9f89ea7
Alex Fontanellas <alexfontanellas@hotmail.com>
2017-07-11 15:34:08 +0200

    done

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,12 +1,14 @@
-const express        = require("express");
-const router         = express.Router();
+/* jshint esversion : 6 */
+const express = require("express");
+const router  = express.Router();
 // User model
-const User           = require("../models/user");
+const User    = require("../models/user");
 // Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
+const bcrypt  = require("bcrypt");
+const bcryptSalt = 10;
 const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+const passport  = require("passport");
+
 
 
 
@@ -14,9 +16,51 @@ router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
+router.get("/signup", (req,res,next) => {
+  res.render("passport/signup");
+});
 
+router.post("/signup", (req,res,next) => {
+  let username = req.body.username;
+  let password = req.body.password;
 
+  if(username === "" || password === ""){
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+  User.findOne({username},"username", (err,user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
 
+    const newUser = User({
+      username: username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+});
+
+router.get("/login", (req,res,next) => {
+  res.render("passport/login");
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/private-page",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
 
 
 module.exports = router;
