aa110ce25842ad0266531de5c0ac6f15387dbb86
Emilie G <adamanska@gmail.com>
2017-07-11 15:59:09 +0200

    let's roll

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,15 +14,22 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/passport-local");
 //require the user model
 const User = require("./models/user");
-const session       = require("express-session");
-const bcrypt        = require("bcrypt");
-const passport      = require("passport");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const flash = require("connect-flash");
 
+const authRoutes = require("./routes/auth-routes");
+app.use('/', authRoutes);
 
 
 
+// Passport shit !
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 //enable sessions here
 
@@ -52,13 +59,45 @@ app.use('/', passportRouter);
 
 
 
+passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+});
 
+passport.deserializeUser((id, cb) => {
+    User.findOne({ "_id": id }, (err, user) => {
+        if (err) { return cb(err); }
+        cb(null, user);
+    });
+});
+
+
+//Some crazy passport stuff that must be before the other passport stuff
+passport.use(new LocalStrategy((username, password, next) => {
+    User.findOne({ username }, (err, user) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return next(null, false, { message: "Incorrect username" });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+            return next(null, false, { message: "Incorrect password" });
+        }
+
+        return next(null, user);
+    });
+}));
 
 //passport code here
 
+app.use(session({
+    secret: "our-passport-local-strategy-app",
+    resave: true,
+    saveUninitialized: true
+}));
 
-
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
@@ -67,20 +106,20 @@ app.use('/', passportRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
