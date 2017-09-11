143ce726dae34217ec924dd157674f7c6dcab124
Mark Allen <mark@traversify.com>
2017-09-11 16:02:49 +0200

    Configured passport

--- starter-code/app.js
+++ starter-code/app.js
@@ -20,20 +20,18 @@ const passport      = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const flash = require("connect-flash");
 
-
-
-
-
 //enable sessions here
 
-
-
+app.use(session({
+  secret: "our-passport-local-strategy-map",
+  resave: true,
+  saveUninitialized: true,
+}))
 
 //initialize passport and session here
 
-
-
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -50,20 +48,34 @@ app.use('/', index);
 app.use('/', users);
 app.use('/', passportRouter);
 
-
-
-
-
 //passport code here
 
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
 
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
 
-
-
-
-
-
+    return next(null, user);
+  });
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
