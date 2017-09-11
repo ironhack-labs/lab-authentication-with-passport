ac42b48bf3ebe74db778a97a21533d16de09e84d
Mercedes Egido <meegido@gmail.com>
2017-09-11 15:51:36 +0200

    iteration 1

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -14,9 +14,41 @@ router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
+router.get('/signup', (req, res, next) => {
+  res.render('passport/signup', { errorMessage: "" })
 
+})
 
+router.post('/signup', (req, res, next) => {
+  let userName = req.body.username;
+  let password = req.body.password;
 
+  User.findOne({ username: userName }, (err, user) => {
+    if(err) {return next(err)}
+    if(user !== null) {
+      res.render('passport/signup', {
+        errorMessage: "This user already exists"
+      })
+      return;
+    }
+    if(userName === "" || password === "") {
+      res.render('passport/signup', {
+        errorMessage: "Write a password or username"
+      })
+      return;
+    }
+    let salt = bcrypt.genSaltSync(bcryptSalt);  //esto es el bycriptSalt?
+    let hashPass = bcrypt.hashSync(password, salt); //esto lo aplica al pass?
 
+    const newUser = User({
+      username: userName,
+      password: hashPass
+    })
+    .save()
+    .then(user => res.redirect('/'))
+    .catch(err => res.render("passport/signup", { errorMessage: "Something went wrong"}))
+
+  })
+})
 
 module.exports = router;
