204dc824afb7cf007e3d5ec029cdf9a689f921b9
Luisa Fernanda Salazar Hoyos <luisafernandasalazarhoyos@MacBook-Pro-de-Luisa.local>
2017-09-11 15:50:53 +0200

    done iteracion 1

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -14,7 +14,42 @@ router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
+router.get("/signup",(req, res) => {
+  res.render("passport/signup")
+})
 
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("passport/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("passport/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("passport/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+});
 
 
 
