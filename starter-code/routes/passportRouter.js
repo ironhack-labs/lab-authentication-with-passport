a7e1c40cc551128bd9ff54ed60bfa5f5efcf8192
lele <lele@crackberry.local>
2017-09-11 17:22:47 +0200

    spaghetti code for signin and signup

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -10,9 +10,61 @@ const passport      = require("passport");
 
 
 
-router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+router.get("/private", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("passport/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("passport/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("passport/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("login");
+      }
+    });
+  });
+});
+
+
+router.get("/signup", (req, res, next) =>{
+	res.render("passport/signup");
+});
+
+router.get("/login", (req, res, next) =>{
+	res.render("passport/login", {"message": req.flash("error")});
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "private",
+  failureRedirect: "login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+
 
 
 
