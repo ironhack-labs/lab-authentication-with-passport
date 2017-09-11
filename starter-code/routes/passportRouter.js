3e936d9c2dbfd8a18f9a46823aa268c4f1bfefd3
Danigp <danielentj@gmail.com>
2017-09-11 16:38:19 +0200

    Done

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,7 +1,7 @@
 const express        = require("express");
-const router         = express.Router();
+const router         = require("express").Router();
 // User model
-const User           = require("../models/user");
+const User           = require("../models/User");
 // Bcrypt to encrypt passwords
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
@@ -10,19 +10,64 @@ const passport      = require("passport");
 
 
 
+router.get("/signup",(req,res,next) => {
+  res.render("passport/signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("passport/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("passport/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    console.log("User created");
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    })
+    .save()
+    .then(user => res.redirect('/'))
+    .catch(e => res.render("passport/signup", { message: "Something went wrong" }));
+
+  });
+});
+
+router.get('/login',(req,res) =>{
+  res.render('passport/login');
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+router.post('/logout',(req,res) =>{
+  req.logout();
+  res.redirect("/");
+});
+
+
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/signup", { user: req.user });
-});
-
-app.post('/signup', (req, res) => {
-  let email    = req.body.email;
-  let password = req.body.password;
-
-  res.send(`Email: ${email}, Password: ${password}`);
 });
 
 
