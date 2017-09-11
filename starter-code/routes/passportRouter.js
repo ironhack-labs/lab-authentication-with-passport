dff1fcbc6719a0488c21627f8ca56c9f91cbe6f0
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-11 15:52:10 +0200

    Iteration #1: The Sign-up Feature DONE

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,22 +1,55 @@
-const express        = require("express");
-const router         = express.Router();
-// User model
-const User           = require("../models/user");
-// Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
-const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+const express      = require("express")
+const router       = express.Router()
+const User         = require("../models/user")
+const bcrypt       = require("bcrypt")
+const bcryptSalt   = 10
+const ensureLogin  = require("connect-ensure-login")
+const passport     = require("passport")
+const debug        = require('debug')('app')
 
 
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
-});
+  res.render("passport/private", { user: req.user })
+})
+
+router.get("/signup", (req, res) => {
+  res.render("passport/signup")
+})
+
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" })
+    return
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("passport/signup", { message: "The username already exists" })
+      return
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt)
+    const hashPass = bcrypt.hashSync(password, salt)
+
+    debug("User created")
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    })
+    .save()
+    .then(user => res.redirect('/'))
+    .catch(e => res.render("auth/signup", { message: "Something went wrong" }))
+
+  })
+})
 
 
 
 
-
-
-module.exports = router;
+module.exports = router
