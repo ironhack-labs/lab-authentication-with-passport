8288c11785b0cc9863ef65894f509b92d0aa7a1d
Mercedes Egido <meegido@gmail.com>
2017-09-11 16:27:43 +0200

    iteration 2

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -51,4 +51,16 @@ router.post('/signup', (req, res, next) => {
   })
 })
 
+router.get('/login', (req, res, next) => {
+  res.render('passport/login', {message: req.flash("error")});
+})
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+
 module.exports = router;
