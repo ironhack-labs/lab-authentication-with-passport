a3359b25d84685be6a6c5a4d5f6840bb875ecc9c
Elo <wanang.elodie@hotmail.fr>
2017-10-02 13:09:23 +0200

    PAR - Luida/Elodie DONE

--- starter-code/app.js
+++ starter-code/app.js
@@ -28,6 +28,42 @@ const flash = require("connect-flash");
 
 
 
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+app.use(flash());
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 //initialize passport and session here
 
