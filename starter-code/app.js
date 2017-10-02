30682d184447090849325e7ecc8eb196ed8877ae
Thibaut Davoult <thibaut.davoult@gmail.com>
2017-10-02 14:48:01 +0200

    PAR - Stuart Gardner - Thibaut Davoult - done

--- starter-code/app.js
+++ starter-code/app.js
@@ -26,11 +26,44 @@ const flash = require("connect-flash");
 
 //enable sessions here
 
-
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
 
 
 //initialize passport and session here
 
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
