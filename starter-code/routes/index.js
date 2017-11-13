bb3e305bfcd4c52e2e2740b51d7e1c39d6a5e9de
Sara Morales <saringa@gmail.com>
2017-11-13 16:26:16 +0100

    done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,21 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (req.isAuthenticated()) {
+    res.render("index", {
+      title: "Express",
+      loggedIn: true
+    });
+  } else {
+    res.render("index", {
+      title: "Express",
+      loggedIn: false
+    });
+  }
 });
 
 module.exports = router;
