0ed195348fec201cab65edd29bf5f7f5eb2c3c88
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-13 13:19:21 +0100

    iteration 1 complete

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -7,6 +7,7 @@ const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
 const ensureLogin = require("connect-ensure-login");
 const passport      = require("passport");
+const mongoose      =
 
 
 
@@ -14,7 +15,43 @@ router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
+router.get('/signup', (req, res, next) =>{
+  res.render('passport/signup');
+});
 
+router.post('/signup', (req, res, next) =>{
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username === "" || password === "") {
+    res.render("signup", {
+      errorMessage: "Please, fill all fields"
+    });
+    return;
+  }
+  // if all is filled, search in bbdd
+  User.findOne({
+      "username": username
+    },
+    "username",
+    (err, user) => {
+      if (user !== null) {
+        res.render("/signup", {
+          errorMessage: "The username already exists"
+        });
+        return;
+      }
+      // if not exist, create and encrypt password
+      var hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(bcryptSalt));
+      var newUser = User({
+        username,
+        password: hashPass
+      });
+
+      newUser.save((err) => {
+        res.redirect("/signup");
+      });
+    });
+});
 
 
 
