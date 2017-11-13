8a7c9ce48fd5b618d82f0ae270a136aae61d913d
Abraham Castro <cucharadepalo@gmail.com>
2017-11-13 15:13:47 +0100

    Private page

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -13,7 +13,7 @@ router.get("/login", (req, res, next) => {
 });
 
 router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
+  successRedirect: "/private-page",
   failureRedirect: "/login",
   failureFlash: true,
   passReqToCallback: true
