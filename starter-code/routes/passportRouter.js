f2318777e1738ed751fc92e5c36d2f9cf1864efa
David Cohen <davidmartincohen@gmail.com>
2018-01-13 10:00:36 +0100

    done

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -8,15 +8,63 @@ const bcryptSalt     = 10;
 const ensureLogin = require("connect-ensure-login");
 const passport      = require("passport");
 
+router.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
+});
 
+router.post("/signup", (req, res, next) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("passport/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("passport/signup", {
+        errorMessage: "The username already exists"
+      });
+      return;
+    }
+
+    var salt     = bcrypt.genSaltSync(bcryptSalt);
+    var hashPass = bcrypt.hashSync(password, salt);
+
+    var newUser = User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("passport/signup", {
+          errorMessage: "Something went wrong when signing up"
+        });
+      } else {
+          res.redirect("/login");
+      }
+    });
+  });
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("passport/login");
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
-
-
-
-
 
 
 module.exports = router;
