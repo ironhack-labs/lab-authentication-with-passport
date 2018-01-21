accd8ed570b206480fad733355208b797569213a
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-01-21 10:34:39 +0100

    Feat: SIgn Up

--- starter-code/controllers/passport.controller.js
+++ starter-code/controllers/passport.controller.js
@@ -1,11 +1,50 @@
-// User model
-const User           = require("../models/user");
 // Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
-const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+// const bcrypt         = require("bcrypt");
+// const bcryptSalt     = 10;
+// const ensureLogin = require("connect-ensure-login");
+const passport = require("passport");
+const mongoose = require('mongoose');
+// User model
+const User = require("../models/user");
 
 module.exports.ensureLoggedIn = (req, res, next) => {
-  res.render("passport/private", { user: req.user });
+  res.render("passport/private", {
+    user: req.user
+  });
+};
+module.exports.signup = (req, res, next) => {
+  res.render("passport/signup");
+};
+module.exports.doSignup = (req, res, next) => {
+  User.findOne({
+      username: req.body.username
+    })
+    .then(user => {
+      if (user != null) {
+        res.render('auth/signup', {
+          user: user,
+          error: {
+            username: 'Username already exists'
+          }
+        });
+      } else {
+        user = new User(req.body);
+        user.save()
+          .then(() => {
+            req.session.currentUser = user;
+            res.render("passport/private", {
+              user: user
+            });
+          }).catch(error => {
+            if (error instanceof mongoose.Error.ValidationError) {
+              res.render('auth/signup', {
+                user: user,
+                error: error.errors
+              });
+            } else {
+              next(error);
+            }
+          });
+      }
+    }).catch(error => next(error));
 };
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -3,6 +3,8 @@ const router         = express.Router();
 const passportController = require('../controllers/passport.controller');
 
 router.get('/private-page', passportController.ensureLoggedIn);
+router.get('/signup', passportController.signup);
+router.post('/signup', passportController.doSignup);
 
 // router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
 //   res.render("passport/private", { user: req.user });
