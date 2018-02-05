05f76179a5e59cbfd50a85180e452629c7d8a12d
Jeff Campigli <jfcampigli@gmail.com>
2018-02-05 13:05:48 +0100

    to step #2

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,22 +1,44 @@
-const express        = require("express");
-const router         = express.Router();
+const express = require("express");
+const router = express.Router();
 // User model
-const User           = require("../models/user");
+const User = require("../models/user");
 // Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
-
-
+const passport = require("passport");
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+router.get("/signup", (req, res) => {
+  res.render("passport/signup");
+});
 
-
-
-
-
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const user = {
+    username
+  };
+  //const password = req.body.password;
+  bcrypt.genSalt(bcryptSalt, (err, salt) => {
+    bcrypt.hash(req.body.password, salt, (err, hashed) => {
+      console.log(req.body.password);
+      if (err) {
+        throw err;
+      } else {
+        user.password = hashed;
+        const newUser = new User(user);
+        newUser.save(err => {
+          if (err) {
+            return err;
+          }
+          res.redirect("/");
+        });
+      }
+    });
+  });
+  //const hashPass =
+});
 
 module.exports = router;
