b5b8789169d2c91ee1230fb5bcfde02b168c54c9
kissy89 <stephanie.senoner@gmail.com>
2018-02-06 17:59:47 +0100

    passport setup

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   username: String,
   password: String
 }, {
-  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
-});
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+})
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,22 +1,64 @@
-const express        = require("express");
-const router         = express.Router();
+const express = require('express')
+const authRoutes = express.Router()
+
 // User model
-const User           = require("../models/user");
+const User = require('../models/user')
+
 // Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
-const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+const bcrypt = require('bcrypt')
+const bcryptSalt = 10
+
+const ensureLogin = require('connect-ensure-login')
+
+const passport = require('passport')
+
+authRoutes.get('/signup', (req, res, next) => {
+  res.render('passport/signup')
+})
+
+authRoutes.post('/signup', (req, res, nextt) => {
+  if (req.session.currentUser) {
+    return res.redirect('/')
+  }
+
+  const username = req.body.username
+  const password = req.body.password
+
+  if (user !== null) {
+    res.render('passport/signup', { message: 'The username is taken' })
+    return
+  }
+
+  const salt = bcrypt.genSaltSync(bcryptSalt)
+  const hashPass = bcrypt.hashSync(password, salt)
+
+  const newUser = new User({
+    username,
+    password: hashPass
+  })
+
+  newUser.save((err) => {
+    if (err) {
+      res.render('passport/signup', { message: 'Something went wrong' })
+    }
+  })
+})
+
+authRoutes.get('/login', (req, res, next) => {
+  res.render('auth/login')
+})
+
+authRoutes.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
+  passReqToCallback: true
+}))
 
 
 
-router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
-});
+authRoutes.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render('passport/private', { user: req.user })
+})
 
-
-
-
-
-
-module.exports = router;
+module.exports = authRoutes
