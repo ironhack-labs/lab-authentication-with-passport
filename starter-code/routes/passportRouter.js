c4d9c7ee647c47a484ec1c9eb54d34ffe19d4354
theRealSheng <sheng.huchai@gmail.com>
2018-02-06 19:15:41 +0100

    added signup form

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,22 +1,23 @@
-const express        = require("express");
-const router         = express.Router();
+const express = require('express');
+const router = express.Router();
 // User model
-const User           = require("../models/user");
+const User = require('../models/user');
 // Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
-const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
+const ensureLogin = require('connect-ensure-login');
+const passport = require('passport');
 
-
-
-router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render('passport/private', { user: req.user });
 });
 
+router.get('/signup', (req, res, next) => {
+  res.render('passport/signup');
+});
 
+router.post('/sign-up', (req, res, next) => {
 
-
-
+});
 
 module.exports = router;
