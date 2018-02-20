1a41c9449ddbc69e298793c2a5d2156b63dd592f
Pablo Gomez <gomezpa@bellsouth.net>
2018-02-20 14:19:29 -0500

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,12 @@ var app = express();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-const passportRouter = require("./routes/passportRouter");
+
+
 //mongoose configuration
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/passport-local");
+
 //require the user model
 const User = require("./models/user");
 const session       = require("express-session");
@@ -22,17 +24,12 @@ const flash = require("connect-flash");
 
 
 
-
-
 //enable sessions here
-
-
-
-
-//initialize passport and session here
-
-
-
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
 
 
 // view engine setup
@@ -48,22 +45,50 @@ app.use(express.static(path.join(__dirname, 'public')));
 // require in the routers
 app.use('/', index);
 app.use('/', users);
-app.use('/', passportRouter);
-
-
-
-
+// app.use('/', passportRouter);
 
 //passport code here
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+app.use(flash());
+passport.use(new LocalStrategy({
+passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
 
 
 
 
+//initialize passport and session here
+app.use(passport.initialize());
+app.use(passport.session());
 
 
-
-
-
+const passportRouter = require("./routes/passportRouter");
+// New Routes
+app.use("/", passportRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -9,12 +9,71 @@ const ensureLogin = require("connect-ensure-login");
 const passport      = require("passport");
 
 
+router.get("/signup", (req, res, next) => {
+  res.render("passport/signup"); 
+});
+
+// Route to handle signup form submission
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  
+  // Validation 1 - Checking that the user has provided
+  // a username and a password
+   if (username === "" || password === "") {
+      res.render("passport/signup", { message: "Indicate username and password" });
+      return;
+   }
+
+   User.findOne({ username }, "username", (err, user) => {
+       if (user != null) {
+           res.render("passport/signup", { message: "The username already exists"});
+       }
+
+       const salt = bcrypt.genSaltSync(bcryptSalt);
+       const hashPass = bcrypt.hashSync(password, salt);
+
+       // Generating the user with the encrypted following the 
+       //two lines before performing the encryption
+       const newUser = new User ({
+          username,
+          password: hashPass
+
+       });
+
+       newUser.save((err) => {
+          if (err) {
+            res.render("passport/signup", { message: "Something went wrong" });
+          } else {
+            res.redirect("/");
+          }
+          
+        });
+   });
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("passport/login", { "message": req.flash("error") });
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/private-page",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+
+}));
+
+
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
-
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
 
 
 
