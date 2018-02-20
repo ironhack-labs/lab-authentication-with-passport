1f47d9a7ef6ce38a26bb9308d6394e523ce9cac0
Francis Calizo <francisacalizo@gmail.com>
2018-02-20 14:55:17 -0500

    complete

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,11 @@ var app = express();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-const passportRouter = require("./routes/passportRouter");
+
 //mongoose configuration
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/passport-local");
+mongoose.connect("mongodb://localhost/passport-exercise");
+
 //require the user model
 const User = require("./models/user");
 const session       = require("express-session");
@@ -20,20 +21,12 @@ const passport      = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const flash = require("connect-flash");
 
-
-
-
-
 //enable sessions here
-
-
-
-
-//initialize passport and session here
-
-
-
-
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,22 +41,47 @@ app.use(express.static(path.join(__dirname, 'public')));
 // require in the routers
 app.use('/', index);
 app.use('/', users);
-app.use('/', passportRouter);
-
-
-
 
 
 //passport code here
 
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
 
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
+app.use(flash());
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
 
+    return next(null, user);
+  });
+}));
 
+//initialize passport and session here
+app.use(passport.initialize());
+app.use(passport.session());
 
-
-
-
+// Routes *THIS HAS TO GO BELOW THE PASSPORT MIDDLEWWARE INITIALIZATION
+const passportRouter = require("./routes/passportRouter");
+app.use('/', passportRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
