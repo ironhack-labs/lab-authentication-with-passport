a3610d6b7937793dc6e2e68e337a7bb2c4b0a3f7
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-04-16 18:51:15 +0200

    Iteration 1 completed

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,15 +1,50 @@
-const express        = require("express");
-const router         = express.Router();
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
 // User model
-const User           = require("../models/user");
+const User = require("../models/user");
 // Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+const passport = require("passport");
 
+router.get("/signup", (req, res) => {
+  res.render("passport/signup");
+});
 
+router.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ username })
+    .then(user => {
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      if (user !== null) {
+        const error = {
+          message: "Username already taken",
+          status: "Test",
+          stack: "Test2"
+        };
+        res.render("error", error);
+      } else {
+        const newUser = User({
+          username,
+          password: hashPass
+        });
+
+        return newUser.save();
+      }
+    })
+    .then(newUser => {
+      res.redirect("/");
+    });
+});
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+module.exports = router;
