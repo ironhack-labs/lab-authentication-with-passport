1cc820190b1aab7cb9fa8582f0de89f2da23d4ce
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-04-16 18:16:40 +0200

    Iteration 1 - signup

--- starter_code/app.js
+++ starter_code/app.js
@@ -12,7 +12,7 @@ const path         = require('path');
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/passport-local', {useMongoClient: true})
+  .connect('mongodb://localhost/lab-passport-local', {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
@@ -54,7 +54,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 const passportRouter = require("./routes/passportRouter");
 app.use('/', index);
-app.use('/', passportRouter);
+app.use('/auth', passportRouter);
 
 
 module.exports = app;
