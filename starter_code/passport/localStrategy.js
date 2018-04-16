9152d26eb4b899ed7e0fc527277825d2d1de8927
A-Tr <alvarotrancon@gmail.com>
2018-04-16 23:21:05 +0200

    finalizado ejercicio

--- starter_code/passport/localStrategy.js
+++ starter_code/passport/localStrategy.js
@@ -2,21 +2,17 @@ const passport = require("passport");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const LocalStrategy = require("passport-local").Strategy;
-const flash = require("connect-flash");
 
 passport.use(
   new LocalStrategy((username, password, next) => {
-    User.findOne({ username }, (err, user) => {
-      if (err) {
-        return next(err);
-      }
+    User.findOne({ username }, (user) => {
       if (!user) {
         return next(null, false, { message: "Incorrect username" });
       }
       if (!bcrypt.compareSync(password, user.password)) {
         return next(null, false, { message: "Incorrect password" });
       }
-
+      console.log(user);
       return next(null, user);
     });
   })
