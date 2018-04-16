112966c5fb63b89a726d48444ce10ffeb79dc979
Yago Vega <yagofvv@gmail.com>
2018-04-16 21:25:03 +0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  if(req.user){
+    let user = req.user;
+    res.render("index", {user});
+  }else{
+    res.render("index");
+  }
+ 
 });
 
 module.exports = router;
