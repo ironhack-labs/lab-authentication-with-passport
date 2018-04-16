0eacd34d187d59e1995a21026ed4cf021984044d
Pablo García <heypablete@gmail.com>
2018-04-16 18:49:03 +0200

    init

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -13,3 +13,6 @@ const passport      = require("passport");
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+
+module.exports = router;
