4a76dfe4a20cdde0010db1c3c0ee3859615241e2
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-04-16 23:03:05 +0200

    Done with style

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("index", { user: req.user });
 });
 
 module.exports = router;
--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
-const mongoose = require("mongoose");
 
 //Import User model
 const User = require("../models/user");
@@ -12,52 +11,69 @@ const bcryptSalt = 10;
 
 const ensureLogin = require("connect-ensure-login");
 
-router.get("/signup", (req, res) => {
+router.get("/signup", (req, res, next) => {
   res.render("passport/signup");
 });
 
-router.post("/signup", (req, res) => {
+router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({ username })
-    .then(user => {
-      const salt = bcrypt.genSaltSync(bcryptSalt);
-      const hashPass = bcrypt.hashSync(password, salt);
+  if (username === "" || password === "") {
 
-      if (user !== null) {
-        const error = {
-          message: "Username already taken",
-          status: "Test",
-          stack: "Test2"
-        };
-        res.render("error", error);
-      } else {
-        const newUser = User({
-          username,
-          password: hashPass
-        });
+    res.render("passport/signup", {message: "Indicate username and password"});
+  } else {
+    User.findOne({ username })
+      .then(user => {
+        const salt = bcrypt.genSaltSync(bcryptSalt);
+        const hashPass = bcrypt.hashSync(password, salt);
 
-        return newUser.save();
-      }
-    })
-    .then(newUser => {
-      res.redirect("/");
-    });
+        if (user !== null) {
+          const error = {
+            message: "Username already taken",
+            status: "Test",
+            stack: "Test2"
+          };
+          res.render("error", error);
+        } else {
+          const newUser = User({
+            username,
+            password: hashPass
+          });
+
+          return newUser.save();
+        }
+      })
+      .then(newUser => {
+        res.redirect("/");
+      });
+  }
 });
 
+router.get("/user", (req, res) => {
+  res.render("passport/user");
+});
 router.get("/login", (req, res) => {
   res.render("passport/login");
 });
 
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/private',
-  failureRedirect: '/login',
-  failureFlash: false 
-}));
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/private",
+    failureRedirect: "/login",
+    failureFlash: false
+  })
+);
 
 router.get("/private", ensureLogin.ensureLoggedIn(), (req, res) => {
+  console.log("ensure login");
   res.render("passport/private", { user: req.user });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
 
 module.exports = router;
