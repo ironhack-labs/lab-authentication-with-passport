3647894e69bec97aad4be22b2a985b5cab07154f
Jorge Terol <jorgetd92@gmail.com>
2018-04-17 21:24:31 +0200

    BCN - Jorge

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,15 +1,60 @@
-const express        = require("express");
-const router         = express.Router();
+const express = require('express');
+const router = express.Router();
 // User model
-const User           = require("../models/user");
+const User = require('../models/user');
 // Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
-const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
+const ensureLogin = require('connect-ensure-login');
+const passport = require('passport');
 
-
-
-router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render('passport/private', { user: req.user });
 });
+
+router.get('/signup', (req, res, next) => {
+  res.render('passport/signup');
+});
+
+router.post('/signup', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    res.redirect('/signup');
+    return;
+  }
+
+  User.findOne({ 'username': username })
+    .then((result) => {
+      if (!result) {
+        const salt = bcrypt.genSaltSync(bcryptSalt);
+        const hashPass = bcrypt.hashSync(password, salt);
+
+        const user = new User({
+          username,
+          password: hashPass
+        });
+
+        user.save()
+          .then(() => {
+            res.redirect('/');
+          });
+        return;
+      }
+      res.redirect('/signup');
+    })
+    .catch(next);
+});
+
+router.get('/auth/google', passport.authenticate('google', {
+  scope: ['https://www.googleapis.com/auth/plus.login',
+    'https://www.googleapis.com/auth/plus.profile.emails.read']
+}));
+
+router.get('/auth/google/callback', passport.authenticate('google', {
+  failureRedirect: '/',
+  successRedirect: '/private-page'
+}));
+
+module.exports = router;
