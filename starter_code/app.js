51ea486804695fd394d2c167e86723ce647007cd
Elise Griset <elise.griset@gmail.com>
2018-04-18 15:11:24 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,60 +1,100 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const User = require('./models/user');
 
+const session = require('express-session');
+const passport = require('passport');
 
 mongoose.Promise = Promise;
 mongoose
   .connect('mongodb://localhost/passport-local', {useMongoClient: true})
   .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.log('Connected to Mongo!');
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err);
   });
 
 const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const debug = require('debug')(
+  `${app_name}:${path.basename(__filename).split('.')[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true,
+  })
+);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(
+  session({
+    secret: 'secret different for every app',
+    saveUninitialized: true,
+    resave: true,
+  })
+);
 
+// PASSPORT LOCAL STRATEGY
 
+passport.serializeUser((userDetails, done) => {
+  console.log('serialize (save to session)');
+  done(null, userDetails._id);
+});
+
+passport.deserializeUser((idFromSession, done) => {
+  console.log('deserialize (details from session)');
+  User.findById(idFromSession)
+    .then(userDetails => {
+      done(null, userDetails);
+    })
+    .catch(err => {
+      done(err);
+    });
+});
+
+function passportSetup(app) {
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use((req, res, next) => {
+    res.locals.oneUser = req.user;
+    next();
+  });
+}
+
+passportSetup(app);
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
 const index = require('./routes/index');
-const passportRouter = require("./routes/passportRouter");
+const passportRouter = require('./routes/passportRouter');
 app.use('/', index);
 app.use('/', passportRouter);
 
-
+module.exports = passportSetup;
 module.exports = app;
--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,15 +1,79 @@
-const express        = require("express");
-const router         = express.Router();
-// User model
-const User           = require("../models/user");
-// Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
-const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+// const bcryptSalt = 10;
+const ensureLogin = require('connect-ensure-login');
+const passport = require('passport');
 
-
-
-router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render('passport/private', {user: req.user});
 });
+
+//  ROUTE TO CREATE AND REDIRECT TO THE SIGNUP PAGE
+router.get('/signup', (req, res, next) => {
+  res.render('../views/passport/signup.hbs');
+});
+
+//  ROUTE TO SAVE USERS CREDENTIALS AND REDIRECT TO HOME PAGE IF CONNECTED
+
+router.post('/process-signup', (req, res, next) => {
+  const {username, password} = req.body;
+
+  if (password === '') {
+    res.redirect('/signup');
+    return;
+  }
+
+  const salt = bcrypt.genSaltSync(10);
+  const encryptedPassword = bcrypt.hashSync(password, salt);
+
+  User.create({username, encryptedPassword})
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// ROUTE TO CREATE AND REDIRECT TO THE LOGIN PAGE
+
+router.get('/login', (req, res, next) => {
+  res.render('../views/passport/login.hbs');
+});
+
+//  ROUTE TO LOG IN
+
+router.post('/process-login', (req, res, next) => {
+  const {username, password} = req.body;
+
+  User.findOne({username})
+    .then(userDetails => {
+      if (!userDetails) {
+        res.redirect('/login');
+        return;
+      }
+      const {encryptedPassword} = userDetails;
+      console.log(password);
+      console.log(userDetails);
+      console.log(encryptedPassword);
+      console.log(bcrypt.compareSync(password, encryptedPassword));
+      if (!bcrypt.compareSync(password, encryptedPassword)) {
+        console.log(
+          'yolo : ',
+          !bcrypt.compareSync(password, encryptedPassword)
+        );
+        res.redirect('/login');
+        return;
+      }
+      req.login(userDetails, () => {
+        res.redirect('/');
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+module.exports = router;
