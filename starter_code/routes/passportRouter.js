f8cb3adfd8c25329176df7d50dfc542c386b79ce
lenniez <lenniezhu@gmail.com>
2018-04-18 08:13:51 +0200

    10% - still figuring out how to reconfigure the Google API to use personal credentials instead of public - I will revisit this later

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -7,11 +7,42 @@ const User = require('../models/user');
 // Bcrypt to encrypt passwords
 const bcrypt = require('bcrypt');
 const bcryptSalt = 10;
-const ensureLogin = require('connect-ensure-login');
+// const ensureLogin = require('connect-ensure-login');
 const passport = require('passport');
+const googleStrategy = require('passport-google').Strategy;
 
-router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
-});
+// middleware
+// router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
+//   res.render('passport/private', { user: req.user });
+// });
+
+//WIP - figuring out how to reconfigure Google Plus API to use personal data instead of public...
+passport.use(new GoogleStrategy({
+  clientID: "1cb0a63e6b28f0e4bc80e48918b3519837361e64",
+  clientSecret: "your Google client secret here",
+  callbackURL: "/auth/google/callback"
+  // returnURL: 'http://localhost:3000/auth/google/return',
+  // realm: 'http://localhost:3000/'
+  },
+  (identifier, done) => {
+    User.findByOpenID({ openId: identifier }, 
+      (err, user) => {
+      return done(err, user);
+    });
+  }
+  const newUser = new User({
+    facebookID: profile.id
+  });
+
+  newUser.save((err) => {
+    if (err) {
+      return done(err);
+    }
+    done(null, newUser);
+  });
+  });
+
+}));
+));
 
 module.exports = router;
