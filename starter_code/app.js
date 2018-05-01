e366838dd27e41a344c11e968d9a38bfa08fe1a9
Diego Leal <diegoleallinux@gmail.com>
2018-05-01 12:28:00 -0400

    finished

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,7 +8,12 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const passport = require('passport');
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/user");
+const bcrypt = require("bcrypt");
 
 mongoose.Promise = Promise;
 mongoose
@@ -38,17 +43,54 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
       
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+ }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+//passport config area
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
 
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 const index = require('./routes/index');
