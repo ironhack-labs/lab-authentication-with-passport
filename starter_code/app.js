c6af509a458963b714b1bea4cb1cb433a39d11d2
rawanalfaiz <rawan.alfaiz@outlook.com>
2018-05-01 18:48:50 -0400

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,6 +8,13 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session      = require("express-session");
+const bcrypt       = require("bcrypt");
+const passport     = require("passport");
+const LocalStrategy = require("passport-local").Strategy;  
+const app          = express();
+const flash        = require("connect-flash");
+const User         = require("./models/user");
 
 
 mongoose.Promise = Promise;
@@ -19,16 +26,59 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+  const app_name = require('./package.json').name;
+  const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+  
+  
+  // Middleware Setup
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: 
+    false }));
+    app.use(cookieParser());
+    app.use(session({
+      secret: "our-passport-local-strategy-app",
+      resave: true,
+      saveUninitialized: true
+    }));
+    
+    
+    app.use(passport.initialize());
+    app.use(passport.session());
+    // app.use(flash());
+    //PASSPORT STRATEGY
+    passport.serializeUser((user, cb) => {
+      cb(null, user._id);
+    });
+    
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
-const app = express();
 
-// Middleware Setup
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(flash());
+
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+    
+    return next(null, user);
+  });
+}));
+
 
 // Express View engine setup
 
@@ -37,7 +87,6 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
