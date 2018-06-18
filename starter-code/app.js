e650231ccfdbc63114e2fe406f4d2be604796f5a
silvio-galli <silvio.galli@gmail.com>
2018-06-18 17:56:21 +0200

    First commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,7 +12,7 @@ const path         = require('path');
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/passport-local', {useMongoClient: true})
+  .connect('mongodb://localhost/starter-code', {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
@@ -52,9 +52,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 const index = require('./routes/index');
-const passportRouter = require("./routes/passportRouter");
 app.use('/', index);
-app.use('/', passportRouter);
 
 
 module.exports = app;
