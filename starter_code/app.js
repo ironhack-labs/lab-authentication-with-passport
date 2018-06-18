4d18285f583ff893f3abef7db8142f8c3f301c29
John Fish <j31@users.noreply.github.com>
2018-06-18 20:41:23 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,18 +1,25 @@
 require('dotenv').config();
 
+const session       = require("express-session");
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
 const hbs          = require('hbs');
 const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const logger        = require('morgan');
+const path          = require('path');
+const bcrypt        = require("bcrypt");
+const passport      = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const flash         = require("connect-flash");
+// const FbStrategy    = require('passport-facebook').Strategy;
 
+const User = require("./models/user");
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/starter-code', {useMongoClient: true})
+  .connect('mongodb://localhost/passport-authentication', {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
@@ -44,15 +51,60 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Passport Authentication';
 
+
+// session
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, cb) => {
+  console.log("CALL OF passport.serializeUser")
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  console.log("CALL OF passport.deserializeUser")
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+app.use(flash());
+passport.use(new LocalStrategy((username, password, next) => {
+  console.log("CALL OF passport.use of LocalStrategy")
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username or password" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect username or password" });
+    }
+
+    return next(null, user);
+  });
+}));
 
 
 const index = require('./routes/index');
 app.use('/', index);
+
+const authRoutes = require('./routes/passportRouter');
+app.use('/', authRoutes);
+
 
 
 module.exports = app;
