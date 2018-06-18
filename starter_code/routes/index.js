875a9202bc144e36914444288d7c5064933664d7
John Fish <j31@users.noreply.github.com>
2018-06-18 21:38:21 +0200

    with bonus feature

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -3,7 +3,13 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  let controls = ""
+  if (req.user) {
+    controls = '<a href="/logout">Log out</a>'
+} else {
+  controls = '<a href="/signup">Sign Up</a> | <a href="/login">Login</a>'
+}
+  res.render('index', {controls});
 });
 
 module.exports = router;
