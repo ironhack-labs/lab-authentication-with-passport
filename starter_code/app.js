51239b7c49155efa23bc8bce0538f89a7d1cc8ad
fodon11 <francisco.odon11@gmail.com>
2018-06-21 22:11:28 -0400

    Finished assignment. Don't feel like I learned much about passport though. Hopefully a future lab will enforce passport a little better

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,7 +8,11 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const passport     = require('passport');
+const session      = require('express-session');
+const LocalStrategy= require('passport-local').Strategy;
+const User         = require('./models/user');
+const bcrypt       = require('bcrypt');
 
 mongoose.Promise = Promise;
 mongoose
@@ -29,6 +33,41 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  secret: 'our-passport-local-strategy-app',
+  resave: true,
+  saveUninitialized: true
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) {return cb(err);}
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({username}, (err, user) => {
+    if(err) {
+      return next(err);
+    }
+    if(!user){
+      return next(null, false, {message: 'Incorrect username'});
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, {message: 'incorrect password'});
+    }
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Express View engine setup
 
@@ -52,9 +91,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 const index = require('./routes/index');
-const passportRouter = require("./routes/passportRouter");
+const passportRoute = require("./routes/passportRouter");
 app.use('/', index);
-app.use('/', passportRouter);
-
+app.use('/', passportRoute);
 
 module.exports = app;
--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,15 +1,64 @@
-const express        = require("express");
+const express        = require('express');
 const router         = express.Router();
 // User model
 const User           = require("../models/user");
 // Bcrypt to encrypt passwords
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
-const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+const ensureLogin    = require("connect-ensure-login");
+const passport       = require("passport");
 
 
+router.get('/signup', (req, res, next) => {
+  res.render('passport/signup');
+});
+
+router.post('/signup', (req, res, next) => {
+  const {username, password} = req.body;
+  User.findOne({'username': username})
+    .then(user => {
+      if (user == null){
+        const salt = bcrypt.genSaltSync(bcryptSalt);
+        const hash = bcrypt.hashSync(password, salt);
+
+        const newUser = new User({
+          username,
+          password: hash
+        });
+        newUser.save()
+          .then(user => {
+            // res.send(user)
+            // res.render('index', {user});
+            res.redirect('/');
+          })
+          .catch(err => {
+            res.render('passport/signup', {errorMessage: 'Something went wrong, try again'});
+          });
+      } else{
+        // res.send(user[0]);
+        res.render('passport/signup', {errorMessage: 'Username already exists'});
+      }
+
+    })
+    .catch(err => {
+      console.log('You be getting some errors SON: ', err);
+    });
+  
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('passport/login');
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
+  passReqToCallback: true
+}));
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+module.exports = router;
