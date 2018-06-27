26b6c9c1324851e828c06e1123e22a49137a056d
Edward Leslie <edwardleslie09@gmail.com>
2018-06-27 16:46:26 -0400

    completed assignment.

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const ensureLogin = require("connect-ensure-login");
+const session = require("express-session");
+const passport = require("passport");
+
+
 // User model
 const User = require("../models/user");
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
-const ensureLogin = require("connect-ensure-login");
-const passport = require("passport");
 
 router.get("/signup", (req, res) => {
   res.render("passport/signup")
 });
 
-router.post("signup", (req, res) => {
+router.post("/signup", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
   if (username === "" || password === "") {
     res.render("passport/signup", {
-      message: "Username and password cannot be empty."
+      message:"The username and password cannot be empty."
     });
     return;
   }
@@ -44,7 +47,7 @@ router.post("signup", (req, res) => {
     newUser.save((err) => {
       if (err) {
         res.render("passport/signup", {
-          message: "Error: Could not save username."
+          message: "Error: Could not save User."
         });
       } else {
         res.redirect("/");
@@ -61,3 +64,25 @@ router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
     user: req.user
   });
 });
+
+router.get("/login", (req, res, next) => {
+  res.render("passport/login");
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("private", { user: req.user });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
+
+module.exports = router;
