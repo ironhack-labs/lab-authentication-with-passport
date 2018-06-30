a7bfce5d5a6f51bdf9d4ea6e5f8840febf50dae8
Teresa Plaz <teresaplaz@gmail.com>
2018-06-30 01:43:12 -0400

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,7 +8,12 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const bcrypt       = require('bcrypt');
+const session      = require('express-session');
+const passport     = require("passport");
+const LocalStrategy= require("passport-local").Strategy;
+const User         = require("./models/user");
+const flash        = require("connect-flash");
 
 mongoose.Promise = Promise;
 mongoose
@@ -29,7 +34,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -44,6 +53,39 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user); 
+  });
+});
+
+
+  app.use(flash());
+  passport.use(new LocalStrategy({
+    passReqToCallback: true
+  }, (req, username, password, next) => {
+    User.findOne({ username }, (err, user) =>  {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username!!!" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // default value for title local
