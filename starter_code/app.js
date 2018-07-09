824403251c2ec1af519f75e2039f26b1fef14ed0
Pablo Rezola <pablorezola20@yahoo.es>
2018-07-09 20:38:41 +0200

    finishing

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("connect-flash");
 
 
 mongoose.Promise = Promise;
@@ -29,11 +32,21 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+  }));
+  app.use(flash());
+  require('./passport')(app);
 
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
@@ -45,9 +58,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.use((req,res,next) => {
+    // default value for title local
+    res.locals.title = 'Express - Generated with IronGenerator';
+    res.locals.user = req.user;
+    res.locals.message = req.flash("error");
+    next();
+  }); 
 
 
 
