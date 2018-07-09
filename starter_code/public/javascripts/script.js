734561e4b9175000c87d9e55cf285315f54e85cc
Leismale <leismale@gmail.com>
2018-07-09 21:28:15 +0200

    Strengh.js added

--- starter_code/public/javascripts/script.js
+++ starter_code/public/javascripts/script.js
@@ -1,5 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-  console.log('IronGenerator JS imported successfully!');
-
-}, false);
+$(document).ready(function($) {
+  $("#password").strength({
+    strengthClass: "strength",
+    strengthMeterClass: "strength_meter",
+    strengthButtonClass: "button_strength",
+    strengthButtonText: "Show Password",
+    strengthButtonTextToggle: "Hide Password"
+  });
+});
