10dbb0acda54836d0e8d68d53bf49cadd9ac70f4
Abel Alonso <abel.alonso@gmail.com>
2018-07-09 19:16:01 +0200

    finished

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -10,6 +10,47 @@ const passport      = require("passport");
 
 
 
+router.get("/signup", (req, res, next) =>{
+  res.render("passport/signup");
+});
+
+router.post("/signup", (req, res, next) =>{
+  const {username, password} = req.body;
+  console.log(username, password);
+  User.findOne({username})
+    .then( user => {
+      if(user!==null) {throw new Error("Username already exists!!!");}
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+      const newUser = new User({username, password: hashPass});
+      console.log(newUser);
+      return newUser.save();
+    }).then(user => {
+      res.redirect("/");})
+    .catch( err => {
+      console.log(err);
+      res.render("/");
+    })
+});
+
+router.get("/login", (req, res, next) =>{
+  res.render("passport/login");
+});
+
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+router.get("/logout", (req, res, next) =>{
+  req.logout();
+  res.redirect("/");
+});
+
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+module.exports = router;
