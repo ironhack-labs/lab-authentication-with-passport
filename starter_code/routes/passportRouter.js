bb0f3352697e51ec6ea5348720077cd0e4ccb0f5
Paula <paulagarizcun@gmail.com>
2018-07-09 18:50:53 +0200

    I1

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -13,3 +13,47 @@ const passport      = require("passport");
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+router.get('/signup', (req, res, next) => {
+  res.render('passport/signup');
+});
+
+router.post('/signup', (req, res, next) => {
+
+  const {
+    username,
+    password
+  } = req.body;
+
+  User.findOne({
+      username
+    })
+    .then(user => {
+      console.log(user);
+      if (user !== null) {
+        throw new Error("Username Already exists");
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        username,
+        password: hashPass
+      });
+
+      return newUser.save()
+    })
+    .then(user => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      console.log(err);
+      res.render("passport/signup", {
+        errorMessage: err.message
+      });
+    })
+})
+
+
+module.exports = router;
