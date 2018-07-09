a90447686315029ac02cf519f99082fcffe13807
Verónica Reina <veroreinah@gmail.com>
2018-07-09 17:33:21 +0200

    Iteration #1: The Sign-up Feature

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -12,4 +12,49 @@ router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
+router.get("/signup", (req, res) => {
+  res.render("passport/signup");
+});
+
+router.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+
+  var fieldsPromise = new Promise((resolve, reject) => {
+    if (username === "" || password === "") {
+      reject(new Error("Indicate a username and a password to sign up"));
+    } else {
+      resolve();
+    }
+  });
+
+  fieldsPromise
+    .then(() => {
+      return User.findOne({ username });
+    })
+    .then(user => {
+      if (user) {
+        throw new Error("The username already exists");
+      }
+
+      // Hash the password
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        username,
+        password: hashPass
+      });
+
+      return newUser.save();
+    })
+    .then(user => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      res.render("passport/signup", {
+        errorMessage: err.message
+      });
+    });
+});
+
 module.exports = router;
