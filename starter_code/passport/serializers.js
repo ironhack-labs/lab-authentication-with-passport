2ccc99256f5e7c769b4dd8111020f0308be40035
Niko <nkouuu@hotmail.com>
2018-07-09 18:26:40 +0200

    done

---
+++ starter_code/passport/serializers.js
@@ -0,0 +1,15 @@
+const User = require('../models/User');
+const passport = require('passport');
+
+passport.serializeUser((user, cb) => {
+    cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+    User.findById(id, (err, user) => {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, user);
+    });
+});
