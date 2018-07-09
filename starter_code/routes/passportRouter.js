480775faa6e3ee627fc71569f1b80b1befc1c46c
Anna Hogberg <am.hogberg@gmail.com>
2018-07-09 20:29:13 +0200

    done

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,7 +1,7 @@
 const express        = require("express");
 const router         = express.Router();
 // User model
-const User           = require("../models/user");
+const User           = require("../models/User");
 // Bcrypt to encrypt passwords
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
@@ -9,7 +9,53 @@ const ensureLogin = require("connect-ensure-login");
 const passport      = require("passport");
 
 
-
-router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+router.get("/private", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+router.get("/signup", (req, res, next) => {
+  res.render("passport/signup")
+});
+
+router.post("/signup", (req, res, next) => {
+  const {username, password} = req.body;
+
+  User.findOne({username})
+    .then(user => {
+      if (user !== null) {
+        throw new Error("Username exists");
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({username,password: hashPass});
+
+      return newUser.save()
+    })
+    .then(user => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      console.log(err);
+      res.render("passport/signup", {
+        errorMessage: err.message
+      });
+    })
+})
+
+router.get('/login', (req, res, next) => {  
+  res.render('passport/login');
+});
+
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "passport/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+)
+
+
+module.exports = router;
+
