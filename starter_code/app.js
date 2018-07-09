a23351928408749e29405de6015310e75cf71720
Dunnia <dunnia.alvarez@gmail.com>
2018-07-10 00:31:32 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser= require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs= require('hbs');
-const mongoose = require('mongoose');
-const logger= require('morgan');
-const path = require('path');
-const dbName =process.env.DBURL;
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
+const dbUrl = process.env.DBURL
 
 
 mongoose.Promise = Promise;
 mongoose
-  .connect(dbName, {useMongoClient: true})
+  .connect(dbUrl, {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
@@ -33,6 +33,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}))
+
+require('./passport')(app);
 
 // Express View engine setup
 
@@ -41,19 +51,14 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
-app.locals.title = 'Example with Passport';
-
-
+app.locals.title = 'Example Passport';
 
 const index = require('./routes/index');
 const passportRouter = require("./routes/passportRouter");
--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,39 +1,84 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 // User model
-const User = require('../models/user');
+const User = require("../models/user");
 // Bcrypt to encrypt passwords
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
-const ensureLogin = require('connect-ensure-login');
-const passport = require('passport');
+const ensureLogin = require("connect-ensure-login");
+const passport = require("passport");
 
-router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-	res.render('passport/private', { user: req.user });
-});
+router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("passport/private", {user: req.user});
+})
 
+//Signup=> get
 router.get('/signup', (req, res, next) => {
-	res.render('passport/signup');
-});
+  res.render('passport/signup')
+})
 
+//Signout =>post
 router.post('/signup', (req, res, next) => {
-	const { username, password } = req.body;
-	User.findOne({ username })
-		.then((user) => {
-			if (user !== null) {
-				throw new Error('username already exists');
-			}
-			const salt = bcrypt.genSaltSync(bcryptSalt);
-			const hashPass = bcrypt.hashSync(password, salt);
+  //Guardamos los datos del formulario
+  const {username, password} = req.body
 
-			const newUser = new User({
-				username,
-				password: hashPass
-			});
-			return newUser.save();
-		})
-		.then((user) => res.redirect('/'))
-		.catch((err) => res.render('passport/signup', {errorMessage: err.message}));
-});
+  //Que no esten vacios
+  let validate = new Promise ((resolve, reject) => {
+    if (username === '') return reject(new Error('Username must be filled'))
+    else if (password === '') return reject(new Error('Password must be filled'))
+    resolve()
+  })
 
-module.exports = router;
+  validate.then(() => {
+       return User.findOne({username})
+  })
+    .then(user => {
+      if (user) {
+        throw new Error('Username already exists')
+      }
+
+      //Si esta vacia creamos el usuario
+      const salt = bcrypt.genSaltSync(bcryptSalt)
+      const hashPass = bcrypt.hashSync(password, salt)
+
+      const newUser = new User({
+        username,
+        password: hashPass
+      })
+
+      return newUser.save()
+    })
+    .then(() => {
+      //Usuario correcto => home
+      res.redirect('/')
+    })
+    .catch(err => {
+      res.render('passport/signup', {errorMessage: err.message})
+    })
+})
+
+//Login get 
+router.get('/login', (req, res, next) => {
+  res.render('passport/login')
+})
+
+//Login post 
+router.post('/login', passport.authenticate('local', {
+    successRedirect: "/",
+    failureRedirect: "/passport/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+)
+//Private get
+router.get('/private', ensureLogin.ensureLoggedIn(), (req, res) => {
+    res.render('passport/private', {user: req.user});
+})
+
+//Logout 
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
+
+module.exports = router
