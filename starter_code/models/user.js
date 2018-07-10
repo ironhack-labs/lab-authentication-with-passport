c4afc3ad24344f5d1a5e1ac2ffc77bfd45a2f767
mabvmex <mabvmex@gmail.com>
2018-07-09 20:14:48 -0500

    done

--- starter_code/models/user.js
+++ starter_code/models/user.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
   username: String,
-  password: String
+  //password: String //Se quita para que no aparezca en la base de datos.
 }, {
   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
 
+userSchema.plugin(passportLocalMongoose)
 const User = mongoose.model("User", userSchema);
+
 module.exports = User;
