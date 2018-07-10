5a1941f396ad1150f7406af08d41c853e8176b25
nallelyht <nallelyht@gmail.com>
2018-07-10 08:47:28 -0500

    DE MEX - Nallely House

--- starter_code/models/user.js
+++ starter_code/models/user.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
   username: String,
-  password: String
 }, {
   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
 
+userSchema.plugin(passportLocalMongoose)
 const User = mongoose.model("User", userSchema);
+
 module.exports = User;
