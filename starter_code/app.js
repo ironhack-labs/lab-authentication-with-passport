528b93c32004356a9eeab5fd6c7bb0f761ae33a7
LauCF <lauracanosaf@hotmail.com>
2018-07-10 09:19:53 +0200

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,7 +8,9 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("connect-flash");
 
 mongoose.Promise = Promise;
 mongoose
@@ -29,7 +31,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
+require("./passport")(app)
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
