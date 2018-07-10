3061a6fb450713fc18c2ed30a6c8c0ba3758d307
Sarai Vasquez <svmarinez@hotmail.com>
2018-07-10 09:10:44 +0200

    Ahora me guarda la session pero no me lleva a la pagina privada.

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -19,7 +19,7 @@ router.get("/login", (req, res, next) => {
 router.post("/login", passport.authenticate("local", {
   successRedirect: "/private-page",
   failureRedirect: "/login",
-  failureFlash: true,
+  failureFlash: false,
   passReqToCallback: true
 }))
 
