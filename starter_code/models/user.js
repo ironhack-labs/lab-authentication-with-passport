5fcb083d5c6d17fbf780a447f9a52feb03bf5cc4
mtrojas <tererojas.i@gmail.com>
2018-07-09 21:20:03 -0500

    done

--- starter_code/models/user.js
+++ starter_code/models/user.js
@@ -1,12 +1,14 @@
+const passportLocalMongoose = require('passport-local-mongoose');
 const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
   username: String,
-  password: String
 }, {
   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
 
+userSchema.plugin(passportLocalMongoose, {usernameField:'username'});
 const User = mongoose.model("User", userSchema);
 module.exports = User;
+
--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,15 +1,34 @@
 const express        = require("express");
 const router         = express.Router();
 // User model
-const User           = require("../models/user");
+const User           = require("../models/User");
 // Bcrypt to encrypt passwords
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
 const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+const passport      = require("../helpers/passport");
 
+router.get("/signup", (req, res) => {
+  res.render("passport/signup");
+})
 
+router.post("/signup", (req, res, next) => {
+  User.register(req.body, req.body.password)
+  .then(user=>res.redirect("login"))
+  .catch(e=>next(e))
+})
+
+router.get("/login", (req, res) => {
+  res.render("passport/login")
+})
+
+router.post("/login", passport.authenticate('local'), (req, res, next) => {
+  res.redirect("/private-page");
+})
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+
+module.exports = router;
