cb647183ff093be9e6525fee65830bd7bedbed33
Lili Cadena <anlica_99@hotmail.com>
2018-07-10 09:15:47 -0500

    Me gusta passport

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -8,8 +8,51 @@ const bcryptSalt     = 10;
 const ensureLogin = require("connect-ensure-login");
 const passport      = require("passport");
 
+function isAuthenticated(req,res,next){
+  if (req.isAuthenticated()){
+      return next()
+  }else{
+      res.redirect("/login");
+  }
+}
 
+function isLoggedIn (req,res,next){
+  if(req.isAuthenticated()){
+      res.redirect("/private");
+  }else{
+      next();
+  }
+}
+
+router.get("/logout", (req,res,next)=>{
+  req.logout();
+  res.redirect("/login");
+})
+
+router.get("/private", isAuthenticated, (req,res)=>{
+  res.render("./passport/private");
+})
+
+router.post("/login", passport.authenticate("local"),(req,res,next)=>{
+  res.redirect("/private")
+})
+
+router.get("/login", isLoggedIn, (req,res)=>{
+  res.render('./passport/login');
+})
+
+router.post("/signup",(req,res,next)=>{
+  User.register(req.body, req.body.password)
+  .then(user=>res.redirect("/login"))
+  .catch(e=>next(e))
+})
+
+router.get("/signup", (req,res)=>{
+  res.render('./passport/signup');
+})
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+module.exports=router;
