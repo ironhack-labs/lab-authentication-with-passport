3786a6c8eeff70dc17534ca97582ca81bcadd9ad
Ruben <rubenvilchesperez1985@hotmail.com>
2018-07-10 08:59:30 +0200

    Ejercicio completado

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -59,7 +59,7 @@ router.get('/login', (req, res, next) => {
 
 router.post("/login", passport.authenticate("local", {
     successRedirect: "/",
-    failureRedirect: "passport/login",
+    failureRedirect: "/login",
     failureFlash: true,
     passReqToCallback: true
   })
