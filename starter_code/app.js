0edda8a9b80ae43dd9b84fdecc2afa05ca68aef0
Jackson Mades <jacksonmades@gmail.com>
2018-07-12 00:09:06 -0400

    I forgot to fork. I think i copied correctly very tired right now

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,16 +8,23 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const app = express();
+const passport = require('passport');
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require('bcryptjs');
+const flash = require('connect-flash');
+const ensureLogin = require('connect-ensure-login');
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/passport-local', {useMongoClient: true})
-  .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
+    .connect('mongodb://localhost/passport-local', {useMongoClient: true})
+    .then(() => {
+        console.log('Connected to Mongo!')
+    }).catch(err => {
     console.error('Error connecting to mongo', err)
-  });
+});
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -33,11 +40,11 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+    src:  path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,15 +1,54 @@
-const express        = require("express");
-const router         = express.Router();
+const express = require("express");
+const router = express.Router();
 // User model
-const User           = require("../models/user");
+const User = require("../models/user");
 // Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
-
+const passport = require("passport");
 
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+    res.render("passport/private", {user: req.user});
 });
+
+router.get("/signup", (req, res, next) => {
+    res.render("views/passport/signup.hbs")
+})
+
+router.post("/signup", (req, res, next) => {
+    const password = req.body.password;
+    const username = req.body.username;
+    if (password === "" || username === "") {
+        res.render('views/passport/signup', {errorMessage: 'Please fill in both a username and password to create an account'})
+        return;
+    }
+    User.findOne({'username': username})
+        .then((responseFromDB) => {
+            if (responseFromDB !== null) {
+                res.render('views/passport/signup', {errorMessage: `Sorry, the username "${username}" is taken`})
+                return;
+            } // ends the if statement
+            const salt = bcrypt.genSaltSync(10);
+            const hashedPassword = bcrypt.hashSync(password, salt);
+            User.create({username: username, password: hashedPassword})
+                .then((response) => {
+                    res.redirect('/');
+                })
+                .catch((err) => {
+                    next(err);
+                })
+        }) // ends the .then from the user.findOne
+});
+
+router.get('/login', (req,res,next)=>{
+    res.render("views/passport/login")
+})
+
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/",//sends to if it worked out
+    failureRedirect: "/login",//sends to if it didnt work out
+    failureFlash: true,
+    passReqToCallback: true
+}));
