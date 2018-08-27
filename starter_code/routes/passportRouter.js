923c14292146e61e64736b7c474d5d06333f7c52
tlebon <tlebon@gmail.com>
2018-08-27 20:31:16 +0200

    hopefully removed nodes

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -13,3 +13,27 @@ const passport      = require("passport");
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+router.get("/signup",(res,req,next)=>{
+  res.render("passport/signup")
+})
+
+router.post('/sign-up', (req, res, next) => {
+  const { email, password } = req.body
+  const encrypted = bcrypt.hashSync(password, 10)
+  new User({ email, password: encrypted })
+      .save()
+      .then(result => {
+          res.send('User account was created')
+      })
+      .catch(err => {
+          if (err.code === 11000) {
+              return res.render('sign-up', { error: 'user exists already' })
+          }
+          console.error(err)
+          res.send('something went wrong')
+      })
+})
+
+
+module.exports = router;
