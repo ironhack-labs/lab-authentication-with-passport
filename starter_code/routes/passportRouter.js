3014a7f7109b5e4ce19b332adde76ed23f1e63e2
Astrid Varga <astrid.varga@gmx.de>
2018-08-27 17:46:04 +0200

    initial commit

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,15 +1,19 @@
-const express        = require("express");
-const router         = express.Router();
+const express = require("express");
+const router = express.Router();
 // User model
-const User           = require("../models/user");
+const User = require("../models/user");
 // Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
-
-
+const passport = require("passport");
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+    res.render("passport/private", { user: req.user });
 });
+
+router.get("/signup", ensureLogin.ensureLoggedIn(), (req, res) => {
+    res.render("signup");
+});
+
+module.exports = router;
