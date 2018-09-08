f017ada738b97c16da8dea74b9617fb53293bced
Max-N3O <maxime.ferhani@gmail.com>
2018-09-08 11:52:46 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
+// User model
+const User           = require("./models/user");
+
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
@@ -8,7 +11,13 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const flash = require("connect-flash");
 
+// Iteration #2: we need to configure Sessions and initialize a session with passport in our app.js file
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 mongoose.Promise = Promise;
 mongoose
@@ -29,6 +38,46 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Iteration #2: we need to configure Sessions and initialize a session with passport in our app.js file:
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+app.use(flash());
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+},(req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Express View engine setup
 
--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -14,4 +14,66 @@ router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
+// Iteration #1: Add a new route to your passportRouter.js file with the path /signup
+//  and point it to your views/passport/signup.hbs file.
+router.get("/signup", (req, res, next)=> {
+  res.render('passport/signup.hbs');
+})
+
+// Iteration #1: Finally, add a post route to your passportRoute to receive the data from the signup form
+// and create a new user with the data.
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+
+  if (username === "" || password === "") {
+    res.render('passport/signup.hbs', { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("passport/signup.hbs", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("passport/signup.hbs", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+});
+
+// Iteration #2: In order to add the login feature, let's add 1 get route to our router to display the login page.
+router.get("/login", (req, res, next)=> {
+  res.render('passport/login.hbs', { "message": req.flash("error") });
+})
+
+// Iteration #2: Once we have the (login) form, let's add another route to the router
+//  to receive that data and log the user in.
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/private-page",
+  failureRedirect: "/login",
+  // failureFlash: true,
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
+
 module.exports = router;
