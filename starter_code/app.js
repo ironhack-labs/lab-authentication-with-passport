324e39e3b285676514ed4ad885624be5f7764503
paulinezenor <paulineronez@gmail.com>
2018-09-08 23:46:53 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const User = require("./models/user");
+
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
@@ -8,6 +10,11 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require('passport');
+const LocalStrategy = require("passport-local").Strategy;
+const flash = require("connect-flash");
 
 
 mongoose.Promise = Promise;
@@ -51,10 +58,55 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+
+app.use(flash());
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+passport.initialize()
+
+
+
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//les lignes ci-dessous doivent toujours être à la fin du app.js
 const index = require('./routes/index');
 const passportRouter = require("./routes/passportRouter");
 app.use('/', index);
 app.use('/', passportRouter);
-
 
 module.exports = app;
--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -10,8 +10,98 @@ const passport      = require("passport");
 
 
 
-router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+router.get("/signup", (req, res) => {
+  res.render("passport/signup");
+});
+
+router.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+  	console.log("ok");
+    res.render("passport/signup", { errorMessage: "Indicate username and password" });
+    return;
+
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("passport/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("passport/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+});
+
+/*router.get("/login", (req, res) => {
+  res.render("passport/login");
+});*/
+
+router.get("/login", (req, res, next) => {
+  res.render("passport/login", { "message": req.flash("error") });
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/private",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+
+router.get("/private", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+/*router.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("passport/login", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username }, (err, user) => {
+      if (err || !user) {
+        res.render("passport/login", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+      	console.log("problem");
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.render("passport/private");
+      } else {
+        res.render("passport/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  });
+});*/
+
+
+
+ensureLogin.ensureLoggedIn()
 
 module.exports = router;
