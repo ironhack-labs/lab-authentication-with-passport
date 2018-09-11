13ffe0baae7d6a7849d6e42653efe7d1fa85c9a2
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-11 09:40:45 -0400

    Added signup route

--- starter_code/models/user.js
+++ starter_code/models/user.js
@@ -1,12 +1,36 @@
 const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
+const Joi = require('joi');
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+  username: {
+      type: String,
+      required: true
+  },
+  password: {
+      type: String,
+      required: true
+  }
 }, {
   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
 
 const User = mongoose.model("User", userSchema);
-module.exports = User;
+
+function validateUser(userData) {
+    let user = {
+        username: Joi.string().required(),
+        password: Joi.string().required()
+    };
+
+    let result = Joi.validate(userData, user);
+    let { error } =  result;
+
+    return error;
+
+}
+
+module.exports = {
+    User,
+    validateUser
+};
--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,7 +1,7 @@
 const express        = require("express");
 const router         = express.Router();
 // User model
-const User           = require("../models/user");
+const { User, validateUser } = require('../models/user');
 // Bcrypt to encrypt passwords
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
@@ -12,6 +12,67 @@ const passport      = require("passport");
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
+});
+
+
+/* Return Signup page */
+router.get('/signup', (req, res, next) => {
+    res.status(200)
+        .render('passport/signup');
+});
+
+/* Create new User */
+router.post('/signup', async function(req, res, next)  {
+
+    let { username, password } = req.body;
+
+    let validateRes = validateUser({
+        username,
+        password
+    });
+
+    let errorMessage = null;
+
+    if(validateRes) {
+        errorMessage = validateRes.details[0].message;
+        return res.status(400)
+            .render('auth/signup', {
+                errorMessage,
+                username,
+                password
+            });
+    }
+
+    let user = await User.findOne({ username });
+
+    if(user) {
+
+        errorMessage = "User with this name already exists!"
+
+        return res.status(400)
+            .render('auth/signup', {
+                errorMessage,
+                username,
+                password
+            });
+
+    } else {
+
+        try {
+            const salt = await bcrypt.genSalt(10);
+            password = await bcrypt.hash(password, salt);
+
+            user = new User({ username, password });
+            const result = await user.save();
+
+            res.status(201)
+                .redirect('/');
+        } catch(ex) {
+            next(ex);
+        }
+
+    }
+
 });
 
 module.exports = router;
