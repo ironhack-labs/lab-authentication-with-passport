4cee07a20aed2dcae45db54fda4a8e78e0444d88
David Sirit <davidsirit@yahoo.com>
2018-09-17 18:06:36 +0200

    Iteration 1 - done

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -14,4 +14,49 @@ router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
+//new route 
+router.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
+});
+
+// formulario
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("passport/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username })
+  .then(user => {
+    if (user !== null) {
+      res.render("/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  })
+  .catch(error => {
+    next(error)
+  })
+});
+
+
+// formulario
 module.exports = router;
