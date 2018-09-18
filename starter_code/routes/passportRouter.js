63deadaea116b11b276b40c3a65e7fa90aff1c04
Ilham Moutaharik <ilham@prescrypto.com>
2018-09-17 20:19:10 -0500

    No estoy segura

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,7 +1,7 @@
 const express        = require("express");
 const router         = express.Router();
 // User model
-const User           = require("../models/user");
+const User           = require("../models/User");
 // Bcrypt to encrypt passwords
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
@@ -13,5 +13,32 @@ const passport      = require("passport");
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+//Signup
+router.get('/signup',(req,res,next)=>{
+  res.render('passport/signup')
+})
+
+router.post('/signup',(req,res,next)=>{
+  User.register(req.body)
+  .then(user=> {
+    res.redirect('/passport/login')
+  }).catch(error=> next(error))
+})
+
+//Login
+router.get('/login',(req,res,next)=>{
+  res.render('passport/login')
+})
+
+router.post('/login', passport.authenticate('local'), (req,res,next)=>{
+  res.redirect('/passport/profile')
+})
+
+//Profile
+router.get('/profile', (req,res,next)=>{
+  res.render('/passport/private')
+})
+
 
 module.exports = router;
