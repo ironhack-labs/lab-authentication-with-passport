d9fd61c3564f86812ddc8f3a7bffded231bec58f
Luther <luther0211@hotmail.com>
2018-09-17 21:49:03 -0500

    done

---
+++ starter_code/helpers/passport.js
@@ -0,0 +1,20 @@
+const passport = require('passport')
+const User = require('../models/user')
+
+//LOCAL STRATEGY
+passport.use(User.createStrategy());
+
+
+//SERIALIZE USER
+passport.serializeUser(function(user,cb){
+  cb(null, user)
+});
+
+//DESERIALIZE USER
+passport.deserializeUser(function(user,cb){
+  cb(null,user)
+}); 
+
+
+
+module.exports = passport 
