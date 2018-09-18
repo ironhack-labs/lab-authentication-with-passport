107f30400fd6323d6bc74c7417a2642505e38343
Francisco Torreblanca <dark1522@gmail.com>
2018-09-17 21:11:00 -0500

    DE MEX - Francisco

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,7 +1,7 @@
 const express        = require("express");
 const router         = express.Router();
 // User model
-const User           = require("../models/user");
+const User           = require("../models/User");
 // Bcrypt to encrypt passwords
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
@@ -13,5 +13,27 @@ const passport      = require("passport");
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+//Routes for signup
+router.get("/signup",(req,res,next)=>{
+  res.render("passport/signup.hbs")
+})
+router.post("/signup",(req,res,next)=>{
+  User.register(req.body,req.body.password)
+    .then(user =>{
+      res.redirect("/login")
+    })
+    .catch(e=>next(e))
+})
+
+//routes for Login
+router.get("/login",(req,res,next)=>{
+  res.render("passport/login.hbs")
+})
+router.post("/login",passport.authenticate("local"), (req,res,next)=>{
+  res.redirect("/private-page")
+})
+
+
 
 module.exports = router;
