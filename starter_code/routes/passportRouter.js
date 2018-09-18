47f1ee2c2c0b75c53da154a7814199a288b56756
Eliot Cano <eliot7cano@gmail.com>
2018-09-18 06:58:38 -0500

    DE Mex Eliot Cano creo que ya acabe pero hay un error que no me deja continuar.

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,14 +1,33 @@
 const express        = require("express");
 const router         = express.Router();
 // User model
-const User           = require("../models/user");
+const User           = require("../models/User");
 // Bcrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
+const bcrypt         = require("bcryptjs");
 const bcryptSalt     = 10;
 const ensureLogin = require("connect-ensure-login");
 const passport      = require("passport");
 
+router.get('/signup',(req,res)=>{
+  res.render('../views/passport/signup.hbs')
+})
 
+router.post('/signup', (req,res,next)=>{
+  User.register(req.body,req.body.password)
+  .then(user=>{
+  res.redirect('/login')
+  })
+  .catch(error=>next(error))
+  })
+
+router.get('/login',(req,res,next)=>{
+  res.render('../views/passport/login.hbs')
+})  
+
+router.post('/login',passport.authenticate('local'),(req,res,next)=>{
+  res.redirect('/profile')
+  })
+  
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
