7e9425445af1978e36fb260048a565f56fe68b1b
danielitocm <danielcomor@gmail.com>
2018-09-18 00:19:19 -0500

    por fin done

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -1,7 +1,7 @@
 const express        = require("express");
 const router         = express.Router();
 // User model
-const User           = require("../models/user");
+const User           = require("../models/User");
 // Bcrypt to encrypt passwords
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
@@ -13,5 +13,35 @@ const passport      = require("passport");
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+
+//RUTA para signup
+
+
+
+
+router.get("/signup",(req,res,next)=>{
+  res.render("../views/passport/signup.hbs")
+})
+router.post("/signup",(req,res,next)=>{
+  User.register(req.body,req.body.password)
+    .then(user =>{
+      res.redirect("/login")
+    })
+    .catch(e=>next(e))
+})
+
+//Ruta para Login
+
+
+
+router.get("/login",(req,res,next)=>{
+  res.render("passport/login.hbs")
+})
+router.post("/login",passport.authenticate("local"), (req,res,next)=>{
+  res.redirect("/private-page")
+})
+
+
 
 module.exports = router;
