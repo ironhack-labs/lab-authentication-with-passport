4d4e54e48a149aa9eb2b4fe69e9d21422b56210f
MGR <mauricio.gzrs@gmail.com>
2018-10-10 09:44:39 -0500

    Authentication with Passport done

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -5,13 +5,54 @@ const User           = require("../models/user");
 // Bcrypt to encrypt passwords
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
-const ensureLogin = require("connect-ensure-login");
-const passport      = require("passport");
+const ensureLogin    = require("connect-ensure-login");
+const passport       = require("passport");
 
+router.get( `/signup`, (req,res) => res.render(`passport/signup`, {signup: true}) );
 
+router.post(`/signup`, (req,res) => {
+  const
+    username = req.body.username,
+    password = req.body.password
+  ;
+  if (username === `` || password === ``) return res.render(`passport/signup`, {errorMessage: `All fields are required`});
+  User
+    .findOne({username})
+    .then(user => {
+      const
+        salt     = bcrypt.genSaltSync(bcryptSalt),
+        hashPass = bcrypt.hashSync(password, salt),
+        newUser = User({
+          username,
+          password: hashPass
+        })
+      ;
+      if (user !== null) return res.render(`passport/signup`, {errorMessage: `User already exists`, signup:true});
+      User
+        .create(newUser)
+        .then( () => res.redirect(`/login`) )
+    })
+  ;
+});
 
-router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+router.get( `/login`, (req,res) => res.render(`passport/login`, {errorMessage: req.flash(`error`), login:true}) );
+
+router.post(`/login`, passport
+  .authenticate(`local`, {
+    successRedirect: `/private-page`,
+    failureRedirect: `/login`,
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
+
+router.get(`/private-page`, ensureLogin.ensureLoggedIn(), (req, res) => res.render("passport/private", { user: req.user }) );
+
+router.get(`/logout`, (req, res) => {
+  req
+    .logout()
+    .redirect("/login")
+  ;
 });
 
 module.exports = router;
