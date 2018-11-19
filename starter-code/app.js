5f2e9858aaf6b7db76658343c3730cc51d76ef94
Bianca Castro <biiianca_00@hotmail.com>
2018-11-19 13:22:30 +0100

    iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -32,11 +32,11 @@ app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
+// app.use(require('node-sass-middleware')({
+//   src:  path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   sourceMap: true
+// }));
       
 
 app.set('views', path.join(__dirname, 'views'));
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -2,9 +2,15 @@ const express        = require("express");
 const passportRouter = express.Router();
 // Require user model
 
+const User= require("../models/user")
+const mongoose= require("mongoose");
+
 // Add bcrypt to encrypt passwords
 
+const bcrypt=require("bcrypt");
+const bcryptSalt=10;
 // Add passport 
+
 
 
 const ensureLogin = require("connect-ensure-login");
@@ -13,5 +19,58 @@ const ensureLogin = require("connect-ensure-login");
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+
+passportRouter.get("/signup", (req,res)=>{
+  res.render("passport/signup");
+})
+
+
+passportRouter.get("/login", (req,res)=>{
+  res.render("passport/login");
+})
+
+
+passportRouter.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username })
+  .then(user => {
+    if (user !== null) {
+      res.render("signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("login");
+      }
+    });
+  })
+  .catch(error => {
+    next(error)
+  })
+});
+
+
+
+
+
 
 module.exports = passportRouter;
