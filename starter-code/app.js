7f8859fb2ce5dd091303f29e4ca49a5d8fafdb7f
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-19 15:42:50 +0100

    Iteration 2 DONE

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,7 +8,11 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const User = require('./models/user.js');
+const bcrypt = require('bcrypt');
 
 mongoose
   .connect(`mongodb://localhost/${process.env.DB}`, {useNewUrlParser: true})
@@ -29,6 +33,41 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Express View engine setup
 
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -7,37 +7,69 @@ const bcrypt = require('bcrypt');
 // Add passport 
 const passport = require('passport');
 
+const session = require('express-session');
+
 const ensureLogin = require("connect-ensure-login");
-
-
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
-});
 
 passportRouter.get("/signup", (req, res) => {
   res.render("passport/signup");
+})
+
+passportRouter.get("/login", (req, res) => {
+  res.render("passport/login");
 });
+
+passportRouter.get("/private", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("passport/private", {user: req.user});
+})
+
+passportRouter.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+})
 
 passportRouter.post("/signup", (req, res) => {
   const saltRounds = 5;
   const { username, password } = req.body;
   if (username === "" || password === "") {
-    res.redirect("passport/signup", { error: "Username or password cant be null" });
+    res.redirect("/signup", { message: "Username or password cant be null" });
     return;
   }
 
-  const salt = bcrypt.genSaltSync(saltRounds);
-  const hash = bcrypt.hashSync(password, salt);
+  User.findOne({ username })
+    .then(found => {
+      if (found !== null) {
+        res.redirect("/signup", { message: "The username already exists" });
+        return;
+      }
 
-  const genericUser = new User({
-    username,
-    password: hash
-  })
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hash = bcrypt.hashSync(password, salt);
 
-  genericUser.save()
-    .then(() => {
-      res.redirect("passport/login")
+      const genericUser = new User({
+        username,
+        password: hash
+      })
+
+      genericUser.save((err) => {
+        if (err) {
+          res.redirect("/signup", { message: "Something went wrong" });
+        } else {
+          res.redirect("/login");
+        }
+      });
     })
-});
+    .catch(error => {
+      next(error)
+    })
+})
+
+passportRouter.post("/login", passport.authenticate("local", {
+  successRedirect: "/private",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
 
 module.exports = passportRouter;
