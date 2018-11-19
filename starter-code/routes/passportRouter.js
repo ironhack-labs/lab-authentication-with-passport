9ce3545024c36e080a0be57501b9d2ac8e4cf9aa
adrigt00 <adriangomez@MacBook-Pro-de-Adrian.local>
2018-11-19 13:03:33 +0100

    iteracion 1

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,8 +1,53 @@
 const express        = require("express");
 const passportRouter = express.Router();
 // Require user model
+router.get('/singup', (req, res, next) => {
+  res.render('views/passport/singup.hbs');
+});
+router.post('/singup', (req, res, next) => {
+  res.render('views/passport/singup.hbs');
+});
 
 // Add bcrypt to encrypt passwords
+authRoutes.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username })
+  .then(user => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  })
+  .catch(error => {
+    next(error)
+  })
+});
+
+module.exports = authRoutes;
+
 
 // Add passport 
 
