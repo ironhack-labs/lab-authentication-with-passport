478502df2aea1b2f3931abddebab5ff4327d5371
AsterDecember <aster.december@gmail.com>
2018-11-26 13:14:17 -0600

    all interactions working

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,13 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const passport     = require("./helpers/passport");
+const session      = require("express-session");
+const MongoStore   = require("connect-mongo")(session);
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/users-pp', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -30,6 +33,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(
+    session({
+        secret: process.env.SECRET,
+        store: new MongoStore({
+            mongooseConnection: mongoose.connection,
+            ttl: 24 * 60 * 60
+        })
+    })
+);
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -37,14 +50,16 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
+//despues de views ponemos passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
@@ -54,7 +69,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 const passportRouter = require("./routes/passportRouter");
-app.use('/', passportRouter);
+app.use('/auth', passportRouter);
 
 
 module.exports = app;
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,16 +1,44 @@
 const express        = require("express");
 const passportRouter = express.Router();
-// Require user model
+const User = require("../models/user");
+const passport = require("passport");
 
 // Add bcrypt to encrypt passwords
 
 // Add passport 
 
+function ensureLoggedIn(req, res, next) {
+    console.log("Usuario?", req.isAuthenticated());
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    return res.redirect("/auth/login");
+}
 
-const ensureLogin = require("connect-ensure-login");
+//const ensureLogin = require("connect-ensure-login");
 
+passportRouter.get('/signup',(req,res,next) => {
+  res.render('passport/signup')
+})
 
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+passportRouter.post('/signup',(req,res,next) =>{
+    User.register(req.body, req.body.password)
+        .then(user => {
+            res.json(user);
+        })
+        .catch(e => next(e));
+})
+
+passportRouter.get('/login',(req,res,next)=>{
+    res.render('passport/login')
+})
+
+passportRouter.post("/login", passport.authenticate("local"), (req, res, next) => {
+    const username = req.user.username;
+    res.send("Tu eres un usuario real con username: " + username);
+});
+
+passportRouter.get("/private-page", ensureLoggedIn, (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
