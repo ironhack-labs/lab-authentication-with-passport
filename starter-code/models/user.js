e65d63ba5f51381d889095a9d428c9f3570a0732
MontseOrtiz <montse_2908@hotmail.com>
2018-11-26 14:32:13 -0600

    MEX PP MONTSE ORTIZ FERMIN LOPEZ

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: true
-});
+const userSchema = new Schema(
+  {
+    username: String,
+    password: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+userSchema.plugin(passportLocalMongoose, { usernameField: "username" });
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,46 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
 // Require user model
-
+const User = require("../models/User");
 // Add bcrypt to encrypt passwords
-
-// Add passport 
-
+// Add passport
+const passport = require("passport");
 
 const ensureLogin = require("connect-ensure-login");
 
-
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+//signup
+passportRouter.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
 });
+
+passportRouter.post("/signup", (req, res, next) => {
+  User.register(req.body, req.body.password)
+    .then(user => {
+      res.json(user);
+    })
+    .catch(e => next(e));
+});
+
+//login
+passportRouter.post(
+  "/login",
+  passport.authenticate("local"),
+  (req, res, next) => {
+    const username = req.user.username;
+    res.send("Bienvenido: " + username);
+  }
+);
+
+passportRouter.get("/login", (req, res, next) => {
+  res.render("passport/login");
+});
+
+passportRouter.get(
+  "/private-page",
+  ensureLogin.ensureLoggedIn(),
+  (req, res) => {
+    res.render("passport/private", { user: req.user });
+  }
+);
 
 module.exports = passportRouter;
