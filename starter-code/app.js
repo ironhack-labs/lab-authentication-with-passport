88bb1b27ddbd62f5ba2ea6e33e0dd7faa945837f
Porfirio Romo <porfirio.romo@yahoo.com.mx>
2019-01-19 10:35:22 -0600

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,16 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require('./models/user')
+const flash = require("connect-flash")
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/lab-passport', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -29,8 +35,45 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret:"pswlocal",
+  resave: true,
+  saveUninitialized: true
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) {return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use( new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({username}, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next( null, false, { message: "Usuario incorrecto"});
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next (null, false, { message: "Contraseña incorrecta"});
+    }
+    return next(null, user);
+  });
+}));
 
 // Express View engine setup
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,5 +1,10 @@
 const express        = require("express");
 const passportRouter = express.Router();
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const bcryptSalt = 10;
 // Require user model
 
 // Add bcrypt to encrypt passwords
@@ -7,11 +12,81 @@ const passportRouter = express.Router();
 // Add passport 
 
 
-const ensureLogin = require("connect-ensure-login");
+passportRouter.get('/signup', (req, res, next) => {
+  res.render('passport/signup')
+})
 
+passportRouter.post('/signup', (req, res, next) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  if(username == "" || password == "") {
+    res.render('passport/sognup', {
+      message: 'Indica tu nombre de usuario y contraseña'
+    })
+    return
+  }
+
+  User.findOne({username})
+  .then(user => {
+    if(user !== null) {
+      res.render('passport/signup', {
+        message: `El usuario ${username} ya se encuentra registrado`
+      })
+      return
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt)
+    const hashPass = bcrypt.hashSync(password, salt)
+
+    const newUser = new User ({
+      username,
+      password: hashPass
+    })
+
+    newUser.save((err) => {
+      if (err) {
+        res.render('passport/signup', {
+          message: 'Algo salió mal y no se pudo guardar tu registro. Intenta de nuevo más tarde'
+        })
+      } else {
+        res.redirect('/')
+      }
+    })
+  })
+  .catch(error => {
+    next(error)
+  })
+})
+
+passportRouter.get('/login', (req, res, next) => {
+  res.render('passport/login')
+})
+
+passportRouter.post("/login", passport.authenticate("local", {
+  successRedirect: "/private-page",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}))
+
+// passportRouter.get("/passport/google", passport.authenticate("google", {
+//   scope: ["https://www.googleapis.com/auth/plus.login", 
+//           "https://www.googleapis.com/auth/plus.profile.emails.read"]
+// }));
+
+// passportRouter.get("/auth/google/callback", passport.authenticate("google", {
+//   failureRedirect: "/",
+//   successRedirect: "/private-page"
+// }));
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
+});
+
+passportRouter.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
 });
 
 module.exports = passportRouter;
