b44db5d3868b7c7b4ef62e43dc7e3c6a172ba0b5
David Casas <cem20903@gmail.com>
2019-02-04 18:28:12 +0100

    Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,6 +8,11 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const session = require('express-session')
+const User = require('./models/user')
+const bcrypt = require('bcrypt')
 
 
 mongoose
@@ -29,6 +34,51 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  secret: "our-passport",
+  resave: true,
+  saveUnitialized: true
+}))
+
+
+passport.use(new LocalStrategy((username, password, next) => {
+  
+  User.findOne({ username }, (err, user) => {
+
+    if (err) {
+      console.log("Error usuario")
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      console.log(password + " " + user.password)
+      return next(null, false, { message: "Incorrect password" });
+    }
+    console.log("Clave correcta")
+    return next(null, user);
+  });
+}));
+
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 // Express View engine setup
 
@@ -53,8 +103,12 @@ app.locals.title = 'Express - Generated with IronGenerator';
 // Routes middleware goes here
 const index = require('./routes/index');
 app.use('/', index);
+
 const passportRouter = require("./routes/passportRouter");
 app.use('/', passportRouter);
+
+
+app.listen(process.env.PORT, ()=>{})
 
 
 module.exports = app;
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,81 @@
 const express        = require("express");
 const passportRouter = express.Router();
-// Require user model
-
-// Add bcrypt to encrypt passwords
-
-// Add passport 
-
-
+const mongoose = require('mongoose')
+const insertUsers  = require('../models/user.js')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
 const ensureLogin = require("connect-ensure-login");
+const flash = require('connect-flash')
+const salt = 10;
+
+
+
+mongoose
+  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+
+
+
 
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+passportRouter.get('/signup',(req,res,next)=>{
+
+res.render('../views/passport/signup')
+})
+
+
+passportRouter.post('/signup',(req,res,next)=>{
+
+  const username = req.body.username
+  const password = req.body.password
+
+  const saltHass = bcrypt.genSaltSync(salt)
+  const hasPass = bcrypt.hashSync(password,saltHass)
+
+  const newUser = {
+    username: username,
+    password: hasPass
+  }
+
+  insertUsers.create(newUser)
+  .then(()=>{
+
+    console.log("Entro")
+
+  })
+})
+
+passportRouter.get('/login',(req,res,next)=>{
+
+  res.render('../views/passport/login')
+})
+
+
+
+passportRouter.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: false,
+  passReqToCallback: true
+}));
+
+
+passportRouter.get('/private', ensureLogin.ensureLoggedIn(), (req,res,next)=>{
+
+
+  res.render('../views/passport/private', {user: req.user})
+})
+
 
 module.exports = passportRouter;
