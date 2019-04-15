77f2a3524856030914c96af95d0a0559fe4d0323
Francisco Vaca <fcovacarh@gmail.com>
2019-04-15 15:41:38 +0200

    Done first iteration

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,14 +1,25 @@
 const express        = require("express");
 const passportRouter = express.Router();
 // Require user model
-
+const User = require("../models/user");
 // Add bcrypt to encrypt passwords
-
+const bcrypt = require("bcrypt");
 // Add passport 
-
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
 
 const ensureLogin = require("connect-ensure-login");
 
+
+passportRouter.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
+});
+
+passportRouter.post("/signup", (req, res, next) => {
+  User.create(req.body)
+  .then(() => res.redirect("/"))
+  .catch(() => res.redirect("/"));
+});
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
