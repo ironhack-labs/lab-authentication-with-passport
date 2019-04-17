a4ca639edad612532fde3b7f941dcd0572d7177e
Julia Álvarez <alvarezjulia1@yahoo.es>
2019-04-17 12:47:25 +0200

    Finished lab

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,102 @@
-const express        = require("express");
-const passportRouter = express.Router();
+const express = require('express')
+const passportRouter = express.Router()
+const bcryptjs = require('bcryptjs')
+const passport = require('passport')
+
 // Require user model
+const User = require('../models/user')
 
 // Add bcrypt to encrypt passwords
 
-// Add passport 
+// Add passport
 
+const ensureLogin = require('connect-ensure-login')
 
-const ensureLogin = require("connect-ensure-login");
+passportRouter.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
+    res.render('passport/private', { user: req.user })
+})
 
+passportRouter.get('/signup', (req, res) => {
+    res.render('passport/signup')
+})
 
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
-});
+passportRouter.post('/signup', (req, res) => {
+    const { username, password } = req.body
+    const salt = bcryptjs.genSaltSync()
+    const hashPassword = bcryptjs.hashSync(password, salt)
 
-module.exports = passportRouter;
+    if (username === '' || password === '') {
+        res.render('passport/signup', {
+            errorMessage: 'You need a username and a password to register'
+        })
+        return
+    }
+
+    User.findOne({ username })
+        .then(user => {
+            if (user) {
+                console.log('Username taken: ', user.username)
+                res.render('passport/signup', {
+                    errorMessage: 'There is already a registered user with this username'
+                })
+                return
+            }
+            User.create({ username, password: hashPassword })
+                .then(() => {
+                    res.redirect('/')
+                })
+                .catch(err => {
+                    console.error('Error while creating user', err)
+                })
+        })
+        .catch(err => {
+            console.error('Error while looking for user', err)
+        })
+})
+
+passportRouter.get('/login', (req, res) => {
+    res.render('passport/login', { errorMessage: req.flash('error') })
+})
+
+passportRouter.post(
+    '/login',
+    passport.authenticate('local', {
+        successRedirect: '/private-page',
+        failureRedirect: '/login',
+        failureFlash: true,
+        passReqToCallback: true
+    })
+)
+
+// passportRouter.post('/login', (req, res, next) => {
+//     // console.log('Hello')
+//     const { username, password } = req.body
+//     if (username === '' || password === '') {
+//         res.render('passport/login', {
+//             errorMessage: 'You need a username and a password to login'
+//         })
+//         return
+//     }
+
+//     User.findOne({ username })
+//         .then(user => {
+//             if (!user) {
+//                 res.render('passport/login', {
+//                     errorMessage: 'This username was not found'
+//                 })
+//             }
+//             if (bcryptjs.compareSync(password, user.password)) {
+//                 // req.session.loggedInUser = user
+//                 res.redirect('/')
+//             } else {
+//                 res.render('passport/login', {
+//                     errorMessage: 'Wrong password'
+//                 })
+//             }
+//         })
+//         .catch(err => {
+//             console.error('Error while finding user', err)
+//         })
+// })
+
+module.exports = passportRouter
