be616edafd91166131847bbe05dda37f00687536
Natasha R. Silva <natashairsilva@gmail.com>
2019-04-19 21:21:18 +0100

    Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,7 +8,12 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require('passport');
+const LocalStrategy = require("passport-local").Strategy;
+const User = require('./models/User')
+const flash = require("connect-flash");
 
 mongoose
   .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
@@ -44,11 +49,62 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+// --- Configuration part for login ---
+// Triggered when the log in is successfull (you will never have to change it)
+passport.serializeUser((user, cb) => {
+  console.log('CALL OF serializeUser', user)
+  cb(null, user._id);
+});
 
+// Triggered when a connected user is doing a request (you will never have to change it)
+passport.deserializeUser((id, cb) => {
+  console.log('CALL OF deserializeUser', id)
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+// A strategy is used when the user tries to log in
+app.use(flash()); // Allows flash errors
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, done) => {
+  console.log('CALL OF LocalStrategy', username, password)
+  User.findOne({ username })
+    .then(userFromDb => {
+      if (!userFromDb) {
+        // The login failed and we send `req.flash("error")` to the next page that is `{ message: "Incorrect username" }`
+        done(null, false, { message: "Incorrect username" })
+      }
+      else if (!bcrypt.compareSync(password, userFromDb.password)) {
+        done(null, false, { message: "Incorrect password" })
+      }
+      else {
+        // Log in `userFromDb` 
+        done(null, userFromDb)
+      }
+    })
+    .catch(err => done(err))
+}));
+
+
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+app.use((req,res,next) => {
+  res.locals.connectedUser = req.user
+  next()
+})
 
 // Routes middleware goes here
 const index = require('./routes/index');
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,14 +1,68 @@
 const express        = require("express");
-const passportRouter = express.Router();
+const passport = require("passport");
 // Require user model
-
+const User = require('../models/User')
 // Add bcrypt to encrypt passwords
-
-// Add passport 
-
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+// Add passport
+const passportRouter = express.Router();
 
 const ensureLogin = require("connect-ensure-login");
 
+passportRouter.get('/signup', (req, res, next) => {
+  res.render('passport/signup')
+})
+
+passportRouter.post('/signup', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("passport/signup", {message: "Indicate username and password"});
+    return
+  } 
+
+  User.findOne({ username })
+  .then(user => {
+    if (user !== null) {
+      res.render("passport/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("passport/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  })
+  .catch(error => {
+    next(error)
+  })
+})
+
+passportRouter.get('/login', (req, res, next) => {
+  res.render('passport/login', {
+    message: req.flash("error")
+  })
+})
+
+passportRouter.post('/login', passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
