299cce886bde1a6958675cf8e08040360add0ff2
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-05-20 16:37:40 +0200

    iteration 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,6 +9,13 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 
+const LocalStrategy = require("passport-local").Strategy
+const flash = require("connect-flash");
+const passport = require("passport")
+const session = require("express-session")
+const bcrypt = require("bcrypt")
+
+const User = require("./models/user")
 
 mongoose
   .connect(`mongodb://localhost/${process.env.DB}`, { useNewUrlParser: true })
@@ -23,6 +30,49 @@ const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}))
+
+
+
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id)
+})
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user)
+  })
+})
+
+app.use(flash())
+
+passport.use(new LocalStrategy({ passReqToCallback: true }, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Usuario incorrecto" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Contraseña incorrecta" });
+    }
+
+    return next(null, user);
+  })
+}))
+
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Middleware Setup
 app.use(logger('dev'));
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -6,24 +6,26 @@ const User = require("../models/user")
 const bcrypt = require("bcrypt")
 const bcryptSalt = 15
 // Add passport 
-
+const passport = require("passport")
 
 const ensureLogin = require("connect-ensure-login");
 
+
+//sign up
 passportRouter.get("/signup", (req, res, next) => res.render("../views/passport/signup"))
 
 passportRouter.post("/signup", (req, res, next) => {
   console.log("******* Llegó")
   const { username, password } = req.body
   if (username === "" || password === "") {
-    res.render("/signup", { message: "Indicate username and password" });
+    res.render("../views/passport/signup", { message: "Indicate username and password" });
     return;
   }
 
   User.findOne({ username })
     .then(user => {
       if (user) {
-        res.render("/signup", { message: "The username already exists" });
+        res.render("../views/passport/signup", { message: "The username already exists" });
         return;
       }
 
@@ -43,6 +45,17 @@ passportRouter.post("/signup", (req, res, next) => {
 })
 
 
+//login
+passportRouter.get("/login", (req, res, next) => {
+  res.render("../views/passport/login", { "message": req.flash("error") })
+})
+
+passportRouter.post('/login', passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}))
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
