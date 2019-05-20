b1063f8431ea46389f19dcfdff714c0f10c9410c
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-05-20 16:00:18 +0200

    Iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect(`mongodb://localhost/${process.env.DB}`, { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -33,11 +33,11 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,53 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
 // Require user model
-
+const User = require("../models/user")
 // Add bcrypt to encrypt passwords
-
+const bcrypt = require("bcrypt")
+const bcryptSalt = 15
 // Add passport 
 
 
 const ensureLogin = require("connect-ensure-login");
 
+passportRouter.get("/signup", (req, res, next) => res.render("../views/passport/signup"))
+
+passportRouter.post("/signup", (req, res, next) => {
+  console.log("******* Llegó")
+  const { username, password } = req.body
+  if (username === "" || password === "") {
+    res.render("/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username })
+    .then(user => {
+      if (user) {
+        res.render("/signup", { message: "The username already exists" });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        username,
+        password: hashPass
+      });
+
+      newUser.save()
+        .then(x => res.redirect("/"))
+        .catch(err => res.render("/signup", { message: `Something went wrong: ${err}` }))
+    })
+
+})
+
+
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+
 
 module.exports = passportRouter;
