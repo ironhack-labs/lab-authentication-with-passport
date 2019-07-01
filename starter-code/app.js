187f4838947b2f38702d2755ff6feef131f2c0d5
Francho Garcia <franchog79@gmail.com>
2019-07-01 17:21:42 +0200

    Iteration 1 finished

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,23 +1,27 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+    .connect('mongodb://localhost/AuthWithPassport', { useNewUrlParser: true })
+    .then(x => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch(err => {
+        console.error('Error connecting to mongo', err)
+    });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -30,14 +34,66 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+//Passport Setup
+passport.use(new LocalStrategy({
+    passReqToCallback: true
+}, (req, username, password, next) => {
+    console.log("login")
+    console.log(username)
+    console.log(password)
+
+    User.findOne({
+        username
+    }, (err, user) => {
+        if (err) {
+            return next(err);
+        }
+
+        console.log(user)
+        if (!user) {
+            return next(null, false, {
+                message: "Incorrect username"
+            });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+            return next(null, false, {
+                message: "Incorrect password"
+            });
+        }
+
+        return next(null, user);
+    });
+}));
+
+passport.serializeUser((user, cb) => {
+    console.log("serialize")
+    console.log(user._id)
+    cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+    console.log("deserialize")
+    console.log(id)
+    User.findById(id, (err, user) => {
+        if (err) {
+            return cb(err);
+        }
+        console.log(user)
+        cb(null, user);
+    });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,6 +1,53 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
+const User = require('../models/user')
+
+
 // Require user model
+passportRouter.get("/signup", (req, res) => {
+    res.render("passport/signup");
+});
+
+passportRouter.post("/signup", (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username === "" || password === "") {
+        res.render("/signup", { message: "Indicate username and password" });
+        return;
+    }
+
+    User.findOne({ username })
+        .then(user => {
+            if (user !== null) {
+                res.render("/signup", { message: "The username already exists" });
+                return;
+            }
+
+            const salt = bcrypt.genSaltSync(bcryptSalt);
+            const hashPass = bcrypt.hashSync(password, salt);
+
+            const newUser = new User({
+                username,
+                password: hashPass
+            });
+
+            newUser.save((err) => {
+                if (err) {
+                    res.render("/signup", { message: "Something went wrong" });
+                } else {
+                    res.redirect("/");
+                }
+            });
+        })
+        .catch(error => {
+            next(error)
+        })
+});
+
+
 
 // Add bcrypt to encrypt passwords
 
@@ -11,7 +58,7 @@ const ensureLogin = require("connect-ensure-login");
 
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+    res.render("passport/private", { user: req.user });
 });
 
 module.exports = passportRouter;
