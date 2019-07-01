97b4809665c22bedd80c90a174c14887a4b83a5a
Rafael Ruiz <rafaelruizgallardon@hotmail.com>
2019-07-01 18:08:05 +0200

    done

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -5,8 +5,9 @@ const userSchema = new Schema({
   username: String,
   password: String
 }, {
-  timestamps: true
+  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
 
 const User = mongoose.model("User", userSchema);
+
 module.exports = User;
