904ac750bcbc3345762052e7bcc2a26ed610c196
bichous <bichous07@gmail.com>
2019-07-02 00:55:29 -0500

    all done

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,40 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
+const passport = require("../middleware/passport");
+const {
+  getSignUp,
+  postSignUp,
+  getLogin
+} = require("../controllers/passportControllers");
 // Require user model
 
 // Add bcrypt to encrypt passwords
 
-// Add passport 
-
+// Add passport
 
 const ensureLogin = require("connect-ensure-login");
 
+passportRouter.get(
+  "/private-page",
+  ensureLogin.ensureLoggedIn(),
+  (req, res) => {
+    res.render("passport/private", { user: req.user });
+  }
+);
 
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
-});
+passportRouter.get("/signup", getSignUp);
+passportRouter.post("/signup", postSignUp);
+
+passportRouter.get("/login", getLogin);
+
+passportRouter.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
 
 module.exports = passportRouter;
