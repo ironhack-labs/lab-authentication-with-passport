b66aa9c85ee1842968f5c7f9d74da6b2b8d7aa2b
bryanmej <bryanmej@gmail.com>
2019-07-05 23:43:02 -0500

    authentication done with passport

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const passport     = require('passport')
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
@@ -8,10 +8,16 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session      = require("express-session")
+const connectMongo = require('connect-mongo')(session)
+const LocalStrategy = require("passport-local").Strategy
+const bcrypt       = require('bcrypt')
+const User         = require('./models/user')
+
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/passPortAuth', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -29,6 +35,42 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: "secret",
+  resave: true,
+  saveUninitialized: true,
+  store: new connectMongo({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60
+  })
+}))
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Express View engine setup
 
@@ -43,8 +85,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,12 +1,65 @@
 const express        = require("express");
 const passportRouter = express.Router();
 // Require user model
-
+const User = require('../models/user')
 // Add bcrypt to encrypt passwords
+const bcrypt =require('bcrypt')
 
 // Add passport 
+const passport = require('passport')
 
 
+//Sign up routes
+passportRouter.get('/signup', (req, res, next) => {
+  res.render('passport/signup')
+})
+
+passportRouter.post('/signup', async(req, res, next) => {
+  const username = req.body.username
+  const password = req.body.password
+  const salt = bcrypt.genSaltSync(10)
+const hashPass = bcrypt.hashSync(password, salt)
+
+  if (username === "" || password === "") {
+    res.render("passport/signup", { message: "Indicate username and password" });
+    return;
+  }
+  
+  const users = await User.find({username})
+
+  if(users.length !== 0){
+    return res.render('passport/signup', {
+      message: 'User already exists'
+    })
+  }
+
+  User.create({
+    username,
+    password: hashPass
+  })
+  .then(()=>{
+    res.redirect('/')
+  })
+  .catch(()=>{
+    console.log(error)
+  })
+
+})
+
+//login routes
+passportRouter.get("/login", (req, res, next) => {
+  res.render("passport/login");
+});
+
+passportRouter.post("/login", passport.authenticate("local", {
+  successRedirect: "/private-page",
+  failureRedirect: "passport/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+
+//Protected routes
 const ensureLogin = require("connect-ensure-login");
 
 
