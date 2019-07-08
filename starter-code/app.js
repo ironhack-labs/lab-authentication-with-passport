aa9db4550456ab0e5343be193c0731a3995e047e
Alexandre M. Bonomi <alexandrembonomi@gmail.com>
2019-07-08 20:51:11 -0300

    project done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,16 +8,21 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require('./models/user');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+.connect('mongodb://localhost/lab-auth-passport', {useNewUrlParser: true})
+.then(x => {
+  console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+})
+.catch(err => {
+  console.error('Error connecting to mongo', err)
+});
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -37,13 +42,48 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // default value for title local
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,57 @@
 const express        = require("express");
 const passportRouter = express.Router();
-// Require user model
-
-// Add bcrypt to encrypt passwords
-
-// Add passport 
-
-
+const User = require('../models/user');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
 const ensureLogin = require("connect-ensure-login");
 
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
+});
+
+passportRouter.get('/signup', (req, res, next) => {
+  res.render('../views/passport/signup');
+});
+
+passportRouter.post('/signup', (req, res, next) => {
+  const {username, password} = req.body;
+  
+  User.find({username: username})
+  .then((user) => {
+    if(user) {
+      res.render('../views/passport/signup', {message: "Usuário já cadastrado!"});
+      return;
+    }
+    
+    const hash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+    
+    User.create({username: username, password: hash})
+    .then(() => {
+    res.render('../views/passport/login')
+  })
+    .catch(err => console.log(err));
+  })
+});
+
+passportRouter.get("/login", (req, res, next) => {
+  res.render("../views/passport/login");
+});
+
+passportRouter.post("/login", passport.authenticate("local", {
+  successRedirect: "/private-page",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  res.render("../views/passport/private", { user: req.user });
+});
+
+passportRouter.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
 });
 
 module.exports = passportRouter;
