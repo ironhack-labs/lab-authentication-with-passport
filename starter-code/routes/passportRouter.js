6780d542ac0fc8992f2c4fe67c75b6aed9062a67
kbightyagami <kino_neko_kira@hotmail.com>
2019-07-07 20:43:18 -0500

    PP-FT-MEX Armando Marquez

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,14 +1,60 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
 // Require user model
-
+const User = require("../models/user");
 // Add bcrypt to encrypt passwords
-
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 // Add passport 
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 const ensureLogin = require("connect-ensure-login");
+const flash = require("connect-flash");
 
+
+passportRouter.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
+});
+
+passportRouter.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("passport/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username })
+    .then(user => {
+      if (user !== null) {
+        res.render("passport/signup", { message: "The username already exists" });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      User.create({
+        username,
+        password: hashPass
+      })
+        .then(() => res.redirect("/"))
+        .catch(() => res.redirect("/"));
+    });
+});
+
+passportRouter.get("/login", (req, res, next) => {
+  res.render("passport/login", { message: req.flash("error") });
+});
+
+passportRouter.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
