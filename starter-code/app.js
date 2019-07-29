5c90c58e515cf2cffa225c19df43892c03568401
Carlos Kruger <carloskr@yahoo.com>
2019-07-29 17:03:01 +0200

    [MAD PP]

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,6 +8,13 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session      = require('express-session')
+const passport     = require('passport')
+const flash        = require('connect-flash')
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt        = require('bcrypt')
+const bcryptSalt    = 10
+const User = require('./models/user')
 
 
 mongoose
@@ -45,7 +52,47 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
+// configuracion sesion
+app.use(session({
+  secret: "secretitoshhhh",
+  resave: true,
+   saveUninitialized: true
+ }));
 
+ passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+
+
+
+ app.use(flash())
+
+ app.use(passport.initialize())
+ app.use(passport.session())
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,14 +1,59 @@
 const express        = require("express");
 const passportRouter = express.Router();
 // Require user model
-
+const User = require('../models/user')
 // Add bcrypt to encrypt passwords
-
+const bcrypt = require('bcrypt')
+const bcryptSalt = 10
 // Add passport 
-
+const passport = require('passport')
 
 const ensureLogin = require("connect-ensure-login");
 
+passportRouter.get("/signup",  (req, res, next) => {
+  res.render("passport/signup");
+});
+
+passportRouter.post("/signup",  (req, res, next) => {
+  const {username, password} = req.body
+  if (username === "" || password === "") {
+    res.render("passport/signup", {errorMessage: "Rellena todo"})
+    return
+  }
+
+
+  User.findOne({ username: username })
+     .then(user => {
+       if (user) {
+         res.render("passport/signup", { errorMessage: "El usuario ya existe" });
+         return;
+       }
+     })
+     .catch(err => console.log('ERROR:', err))
+
+      const salt = bcrypt.genSaltSync(bcryptSalt)
+      const hashPass = bcrypt.hashSync(password, salt)
+      User.create({ username, password: hashPass })
+        .then(() => {
+          console.log("creando")
+          res.redirect('/')})
+        .catch(err => console.log('ERROR:', err))
+
+});     
+
+passportRouter.get("/login",  (req, res, next) => {
+  res.render("passport/login", {"message" : req.flash("error")});
+});
+
+passportRouter.post("/login", passport.authenticate("local", {
+   successRedirect: "/private-page",
+   failureRedirect: "/login",
+   failureFlash: true,
+   passReqToCallback: true
+}));
+
+
+  
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
