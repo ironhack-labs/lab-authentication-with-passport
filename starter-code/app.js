77a0843acc07a593b4930921e2bea1faf46bf45c
Carla Rodriguez <carlavrodriguezm@gmail.com>
2019-07-29 13:55:54 +0200

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,9 +9,16 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const session = require('express-session');
+const passport = require('passport');
+const flash = require("connect-flash");
+
+
+require('./configs/passport.config')
+require('./configs/mongoose.config')
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/users-passwords', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -44,6 +51,19 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+// Configuración sesión
+app.use(session({
+  secret: "secretoPssprt",
+  resave: true,
+  saveUninitialized: true
+}))
+
+// Flash error handling middleware
+app.use(flash())
+
+// Iniciacilización de passport y de sesión
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 // default value for title local
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,11 +1,69 @@
 const express        = require("express");
 const passportRouter = express.Router();
+const passport= require('passport')
 // Require user model
 
 // Add bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10
 
 // Add passport 
+const User = require("../models/user");
 
+passportRouter.get("/signup", (req, res, next) => res.render("passport/signup"))
+passportRouter.post("/signup", (req, res, next) => {
+
+    const { username, password } = req.body
+
+    if (username === "" || password === "") {
+        res.render("passport/signup", { message: "Rellena todo" });
+        return;
+    }
+
+    User.findOne({ username })
+        .then(user => {
+            if (user) {
+                res.render("passport/signup", { message: "El usuario ya existe" });
+                return;
+            }
+
+            const salt = bcrypt.genSaltSync(bcryptSalt);
+            const hashPass = bcrypt.hashSync(password, salt);
+
+            const newUser = new User({
+                username,
+                password: hashPass
+            });
+
+            newUser.save((err) => {
+                if (err) {
+                    res.render("passport/signup", { message: "Something went wrong" });
+                } else {
+                    res.redirect("/");
+                }
+            });
+
+        })
+        .catch(error => {
+            next(error)
+        })
+});
+
+passportRouter.get("/login", (req, res, next) => {
+  res.render("passport/login", { "message": req.flash("error") });
+})
+
+passportRouter.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+passportRouter.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
 
 const ensureLogin = require("connect-ensure-login");
 
