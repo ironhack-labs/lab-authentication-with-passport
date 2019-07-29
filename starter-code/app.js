ed6307bf3e4e3902c57eebdab7e5c44d685c917e
Iváne <iromcan1984@gmail.com>
2019-07-29 14:16:57 +0200

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,23 +1,19 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+//const hbs          = require('hbs');
+//const mongoose     = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require("connect-flash");
 
-
-mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+require('./configs/passport.config')
+require('./configs/mongoose.config')
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -33,17 +29,30 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+// Configuración sesión
+app.use(session({
+  secret: "secretoPssprt",
+  resave: true,
+  saveUninitialized: true
+}))
+
+// Flash error handling middleware
+app.use(flash())
+
+// Iniciacilización de passport y de sesión
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 // default value for title local
