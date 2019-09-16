0a272d132730e1e5a7aacc4231e0dbb4cb3d56df
JesusSP <jesus.saints@gmail.com>
2019-09-16 13:18:53 +0200

    First iteration

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,49 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
-// Require user model
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
+// Require user model
+passportRouter.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
+});
+
+passportRouter.post("/signup", (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (username === "" || password === "") {
+    res.render("passport/signup", { message: "You must fill both fields." });
+  }
+
+  User.findOne({ username }).then(userFound => {
+    if (userFound) {
+      res.render("passport/signup", { message: "This user already exists." });
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    User.create({ username, password: hashPass })
+      .then(userCreated => {
+        res.redirect("/login")
+      });
+    
+   
+  });
+});
 // Add bcrypt to encrypt passwords
 
-// Add passport 
-
+// Add passport
 
 const ensureLogin = require("connect-ensure-login");
 
-
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
-});
+passportRouter.get(
+  "/private-page",
+  ensureLogin.ensureLoggedIn(),
+  (req, res) => {
+    res.render("passport/private", { user: req.user });
+  }
+);
 
 module.exports = passportRouter;
