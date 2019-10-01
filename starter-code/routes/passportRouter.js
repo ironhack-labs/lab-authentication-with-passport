5e8684a7d2cc2addeca1f1a1f2807fb53c0fd111
LsM Lucas Martins <lucas.smar@gmail.com>
2019-10-01 14:21:30 -0300

    done

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,14 +1,67 @@
-const express        = require("express");
+const express = require("express");
+
 const passportRouter = express.Router();
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const passport = require("passport");
+const User = require("../models/user");
 // Require user model
 
 // Add bcrypt to encrypt passwords
 
-// Add passport 
-
+// Add passport
 
 const ensureLogin = require("connect-ensure-login");
 
+passportRouter.get("/signup", (req, res) => {
+  res.render("passport/signup");
+});
+passportRouter.post("/signup", (req, res, next) => {
+  console.log(req.body);
+  // const newUser = new userModel(req.body);
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("passport/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }).then(user => {
+    if (user !== null) {
+      res.render("passport/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save(err => {
+      if (err) {
+        res.render("passport/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+});
+passportRouter.get("/login", (req, res) => {
+  res.render("passport/login");
+});
+passportRouter.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
