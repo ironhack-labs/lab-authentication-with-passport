08a9da7d110012dcaa5cf189de8e16c71851086c
adelcanto <a.delcanto@outlook.com>
2019-11-18 15:45:05 +0100

    Iteration 1 - DONE

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,33 @@
 const express        = require("express");
 const passportRouter = express.Router();
 // Require user model
-
+const User = require('../models/user')
 // Add bcrypt to encrypt passwords
-
+const session = require("express-session")
+const bcrypt = require("bcrypt")
+const bcryptSalt = 10;
 // Add passport 
-
+const passport = require("passport")
+const LocalStrategy = require("passport-local").Strategy
 
 const ensureLogin = require("connect-ensure-login");
+const Swag = require('swag');
+Swag.registerHelpers('hbs');
 
+//////////////////////////////////
+//Meter bodyparser/mongoose/hbs/etc ??
+/////////////////////////////////
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
+});
+
+passportRouter.get("/signup", (req, res) => {
+  res.render('passport/signup');
+});
+
+passportRouter.post("/signup", (req, res) => {
+  // res.render('passport/signup');
 });
 
 module.exports = passportRouter;
