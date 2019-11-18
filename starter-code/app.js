484c129e1cf17b67ac932e4e73892b62065963a8
Paolo <fedeli.paolol@gmail.com.com>
2019-11-18 16:26:42 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -7,7 +7,15 @@ const favicon      = require('serve-favicon');
 const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
-const path         = require('path');
+const path = require('path');
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+const passport = require('passport')
+const flash = require('connect-flash')
+const User = require("./models/user");
+const bcrypt = require("bcrypt");
+
+
 
 
 mongoose
@@ -30,6 +38,45 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+
+// PASSPORT: Serializado / des-serializado Usuario
+passport.serializeUser((user, cb) => cb(null, user._id));
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+
+app.use(flash());
+
+
+// PASSPORT: estrategia
+passport.use(new LocalStrategy({ passReqToCallback: true }, (req, username, password, next) => {
+  User.findOne({ username })
+    .then(theUser => {
+      if (!theUser) return next(null, false, { message: "Nombre de usuario incorrecto" })
+      if (!bcrypt.compareSync(password, theUser.password)) return next(null, false, { message: "Contraseña incorrecta" })
+      return next(null, theUser);
+    })
+    .catch(err => next(err))
+}))
+
+
+
+
+  // PASSPORT: configuración sesión
+  app.use(session({ secret: "webmad1019", resave: true, saveUninitialized: true }));
+
+  // PASSPORT: inicialización de sesión
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+
+
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -51,10 +98,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 // Routes middleware goes here
-const index = require('./routes/index');
-app.use('/', index);
-const passportRouter = require("./routes/passportRouter");
-app.use('/', passportRouter);
+app.use("/", require("./routes/index"));
+app.use('/', require("./routes/passportRouter"));
 
 
 module.exports = app;
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,67 @@
 const express        = require("express");
 const passportRouter = express.Router();
+const passport = require("passport");
+
 // Require user model
+const User = require('../models/user')
+
 
 // Add bcrypt to encrypt passwords
+const bcryptSalt = 10
+const bcrypt = require('bcrypt')
 
 // Add passport 
+// const passport = require('passport')
 
+passportRouter.get("/signup", (req, res) => res.render("passport/signup"));
+
+passportRouter.post("/signup", (req, res) => {
+  const { username, password } = req.body
+  
+  if (!username || !password) {
+    res.render("passport/signup", { message: "Introduce un usuario y contraseña" });
+    return;
+  }
+
+
+User.findOne({ username })
+
+    .then(user => {
+      if (user) {
+        res.render("passport/signup", { message: "El usuario ya existe, merluzo" });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      User.create({ username, password: hashPass })
+        .then(x => res.redirect("/"))
+        .catch(x => res.render("passport/signup", { message: "Algo fue mal, inténtalo más tarde. Oopsy!" }))
+    })
+    .catch(error => { next(error) })
+});
+
+//// LOGIN SESSION
 
 const ensureLogin = require("connect-ensure-login");
 
 
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+passportRouter.get("/private", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+
+passportRouter.get('/login', (req, res) => res.render('passport/login', { "message": req.flash("error") }))
+
+
+passportRouter.post('/login', passport.authenticate('local', {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true ///// que es ?^?^?^
+}));
+
+
 
 module.exports = passportRouter;
