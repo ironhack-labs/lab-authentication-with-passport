2ee65452938ca650e37c42c27109ebfba6b0520d
estefania-git <ecasmed@gmail.com>
2019-11-18 18:32:06 +0100

    done

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -5,44 +5,90 @@ const User = require("../models/user");
 // Add bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
-// Add passport 
+// Add passport
+const flash = require("connect-flash");
 
-// const passport = require("passport");
-// const LocalStrategy = require("passport-local").Strategy;
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 const ensureLogin = require("connect-ensure-login");
 
+passport.use(
+  "local-auth",
+  new LocalStrategy(
+    {
+      passReqToCallback: true
+    },
+    (req, username, password, next) => {
+      User.findOne(
+        {
+          username
+        },
+        (err, user) => {
+          // todo: watch with mongodb stopped
+          if (err) {
+            return next(err);
+          }
 
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", {
-    user: req.user
+          if (!user) {
+            return next(null, false, {
+              message: "Incorrect username"
+            });
+          }
+          if (!bcrypt.compareSync(password, user.password)) {
+            return next(null, false, {
+              message: "Incorrect password"
+            });
+          }
+
+          return next(null, user);
+        }
+      );
+    }
+  )
+);
+
+passport.serializeUser((user, cb) => {
+  console.log("serialize");
+  console.log(`storing ${user._id} in the session`);
+  cb(null, user._id);
+  // cb(null, {id: user._id, role: user.role});
+});
+
+passport.deserializeUser((id, cb) => {
+  console.log("deserialize");
+  console.log(`Attaching ${id} to req.user`);
+  // eslint-disable-next-line consistent-return
+  User.findById(id, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
   });
 });
+
+// passportRouter.use(passport.initialize());
+
+//----SIGNUP----
 passportRouter.get("/signup", (req, res) => {
-  res.render("passport/signup")
+  res.render("passport/signup");
 });
 
-
 passportRouter.post("/signup", (req, res, next) => {
-  const {
-    username,
-    password
-  } = req.body;
+  const { username, password } = req.body;
   if (username === "" || password === "") {
     res.render("passport/signup", {
-      message: "Indicate username and password",
-
+      message: "Indicate username and password"
     });
     return;
   }
   User.findOne({
-      username
-    })
-    .then((user) => {
+    username
+  })
+    .then(user => {
       if (user !== null) {
         res.render("passport/signup", {
-          message: "The username already exists",
-
+          message: "The username already exists"
         });
         return;
       }
@@ -52,20 +98,49 @@ passportRouter.post("/signup", (req, res, next) => {
         username,
         password: hashPass
       });
-      newUser.save((err) => {
+      newUser.save(err => {
         if (err) {
           res.render("passport/signup", {
-            message: "Something went wrong",
-
+            message: "Something went wrong"
           });
         } else {
           res.redirect("/");
         }
       });
     })
-    .catch((error) => {
+    .catch(error => {
       next(error);
     });
 });
+passportRouter.get("/login", (req, res) => {
+  res.render("passport/login", {
+    message: req.flash("error")
+  });
+});
+
+//----LOGIN----
+passportRouter.get("/login", (req, res) => {
+  res.render("passport/login");
+});
+
+passportRouter.post(
+  "/login",
+  passport.authenticate("local-auth", {
+    successRedirect: "/private-page",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
+
+passportRouter.get(
+  "/private-page",
+  ensureLogin.ensureLoggedIn(),
+  (req, res) => {
+    res.render("passport/private", {
+      user: req.user
+    });
+  }
+);
 
 module.exports = passportRouter;
