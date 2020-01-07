16c1ae06fdc504bfadec3915820dd9dda8f5f8bb
Vini <mvfv@outlook.com>
2020-01-07 15:14:37 -0300

    Completo

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,16 +1,70 @@
 const express        = require("express");
 const passportRouter = express.Router();
-// Require user model
+const ensureLogin = require("connect-ensure-login");
+const User = require('../models/user')
+const passport = require('passport')
+
 
 // Add bcrypt to encrypt passwords
+const bcrypt = require('bcrypt');
 
 // Add passport 
 
+passportRouter.get("/signup", (req, res) => {
+  res.render("passport/signup");
+});
 
-const ensureLogin = require("connect-ensure-login");
 
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("passport/private", { user: req.user });
+});
+
+
+passportRouter.post('/signup', (req, res, next) => {
+  const {username, password} = req.body;
+  const salt = bcrypt.genSaltSync(10);
+  const hashPass = bcrypt.hashSync(password, salt);
+
+  if (username === "" || password === "") {
+    res.render('passport/signup') 
+    return;
+  }
+
+  User.findOne({username})
+  .then(nameUser => {
+    if (nameUser !== null) {
+        res.render('signup')
+        return;
+    };
+  User.create({
+    username,
+    password: hashPass,
+  })
+  .then(() => {
+    res.redirect('/login');
+  })
+  .catch(error => {
+    console.log(error);
+  })
+  })
+
+});
+
+
+passportRouter.get("/login", (req, res) => {
+  res.render("passport/login", {message:req.flash('error')} );
+});
+
+passportRouter.post("/login", passport.authenticate("local", {
+  successRedirect: "/private", 
+  failureRedirect: "/login", 
+  failureFlash: true, 
+  passReqToCallback: true, 
+ }));
+
+
+passportRouter.get("/private", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
