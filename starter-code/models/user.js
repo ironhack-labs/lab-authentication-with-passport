cbcdd94e7000cca8de33f74c390976f17c44454c
Pilar <p.garciacampo@gmail.com>
2020-02-06 22:53:46 +0100

    First steps iteration 1

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -1,12 +1,18 @@
+/*jshint esversion: 6 */
 const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: true
-});
+const userSchema = new Schema(
+  {
+    username: { type: String, unique: true, index: true },
+    password: String
+  },
+  {
+    timestamps: true
+  }
+);
 
 const User = mongoose.model("User", userSchema);
+User.collection.createIndexes();
+
 module.exports = User;
