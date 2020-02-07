31bc8ec8cc852033cd0fafdb263bf83f59eeda30
pelinski <teresapelinski@gmail.com>
2020-02-07 20:20:10 +0100

    added signup form and route

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,25 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
+
 // Require user model
+const User = require("../models/user");
 
 // Add bcrypt to encrypt passwords
 
-// Add passport 
+// Add passport
 
+passportRouter.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
+});
 
 const ensureLogin = require("connect-ensure-login");
 
-
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
-});
+passportRouter.get(
+  "/private-page",
+  ensureLogin.ensureLoggedIn(),
+  (req, res) => {
+    res.render("passport/private", { user: req.user });
+  }
+);
 
 module.exports = passportRouter;
