4ddaa46d92948877955c877890cd9f65443fcbfc
Peacock-Co <pavaoleonardo@hotmail.com>
2020-02-07 15:10:09 +0100

    First commit, passportRoutes

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,74 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
+
 // Require user model
+const User = require("../models/user");
 
 // Add bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
-// Add passport 
-
+// Add passport
+const passport = require("passport");
 
 const ensureLogin = require("connect-ensure-login");
 
+passportRouter.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
 
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({
+      username: username
+    })
+    .then(user => {
+      if (user !== null) {
+        res.render("auth/signup", {
+          errorMessage: "The username already exists!"
+        });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      User.create({
+          username,
+          password: hashPass
+        })
+        .then(() => {
+          res.redirect("/");
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    })
+    .catch(error => {
+      next(error);
+    });
 });
+
+//GET HOME PAGE
+passportRouter.get("/signup", (req, res, next) => {
+  res.render("views/passport/signup");
+});
+
+passportRouter.get(
+  "/private-page",
+  ensureLogin.ensureLoggedIn(),
+  (req, res) => {
+    res.render("passport/private", {
+      user: req.user
+    });
+  }
+);
 
 module.exports = passportRouter;
