839a5dd3ab122a99da72264a73d3a75d614bbd25
Borja del Amo <borjadao@gmail.com>
2020-02-07 19:38:57 +0100

    iteracion1

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,44 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
 // Require user model
-
+const User = require('../models/user');
 // Add bcrypt to encrypt passwords
-
-// Add passport 
-
-
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
+// Add passport
+const passport = require("passport")
 const ensureLogin = require("connect-ensure-login");
-
-
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
-
+passportRouter.get('/signup', (req, res, next) => {
+  res.render('passport/signup');
+});
+passportRouter.post('/signup', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.render('passport/signup', { message: 'Indicate username and password' });
+    return;
+  }
+  User.findOne({ username })
+    .then(user => {
+      if (user !== null) {
+        res.render('passport/signup', { message: 'The username already exists' });
+        return;
+      }
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+      const newUser = new User({
+        username,
+        password: hashPass
+      });
+      return newUser.save();
+    })
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(error => {
+      res.render('passport/signup', { message: 'Something went wrong' });
+    });
+}); 
 module.exports = passportRouter;
