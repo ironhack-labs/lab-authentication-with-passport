96d02364dd40ce6adc1880789653f1b0bf3f25a9
Gonzalo Hernández <gonzalohernandez@icloud.com>
2020-02-07 21:09:55 +0100

    iteration 1 finished

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,29 @@
-const express        = require("express");
-const passportRouter = express.Router();
-// Require user model
-
-// Add bcrypt to encrypt passwords
-
-// Add passport 
-
-
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+// Add passport
 const ensureLogin = require("connect-ensure-login");
 
+router.get("/signup", (req, res, next) => res.render("passport/signup"));
+router.post("/signup", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const existUser = await User.findOne({ username });
+    if (!existUser) {
+      const cryptPass = bcrypt.hashSync(password, 10);
+      await User.create({ username, password: cryptPass });
+      return res.redirect("/");
+    } else {
+      return res.render("passport/signup");
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
-module.exports = passportRouter;
+module.exports = router;
