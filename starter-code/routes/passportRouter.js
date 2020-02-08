24a061a5f9a28ddee8969a901376055a506e3b32
David Garcia <dgarciabravo13@gmail.com>
2020-02-08 20:34:33 +0100

    working in passport

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,8 +1,29 @@
 const express        = require("express");
 const passportRouter = express.Router();
-// Require user model
+const User = require("../models/user");
+const { hashPassword, checkHashed } = require("../lib/hashing");
+const passport = require("passport");
 
-// Add bcrypt to encrypt passwords
+// Require user model
+passportRouter.get("/signup", (req,res,next) => {
+  //console.log("hola");
+  res.render("passport/signup");
+})
+
+passportRouter.post("/signup", async (req,res,next) => {
+  const { username, password } = req.body;
+  const existUser = await User.findOne({ username });
+  if(!existUser){
+    const NewUser = await User.create({ username, password:hashPassword(password) });
+    console.log("Usuario Creado");
+    res.redirect("/");
+  }
+  
+})
+passportRouter.get("/login", (req,res,next) => {
+  //console.log("hola");
+  res.render("passport/login");
+})
 
 // Add passport 
 
