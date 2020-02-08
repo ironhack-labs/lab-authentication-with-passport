95773fa186dbc2242fa0652c3511ecc0b6189068
Borja Domínguez <borja.d.atienza@gmail.com>
2020-02-08 19:41:16 +0100

    Iterarion 1 DONE

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,42 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
+const ensureLogin = require("connect-ensure-login");
+const model = require("../models/user");
+const passport = require("passport");
+const { hashPassword, checkHashed } = require("../lib/hashing");
 // Require user model
 
 // Add bcrypt to encrypt passwords
 
-// Add passport 
+// Add passport
 
-
-const ensureLogin = require("connect-ensure-login");
-
-
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+passportRouter.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
 });
+
+passportRouter.post("/signup", async (req, res, next) => {
+  const { username, password } = req.body;
+  console.log("username");
+  const userCreated = await model.findOne({ username });
+
+  if (userCreated) {
+    return res.redirect("/signup");
+  } else {
+    await model.create({
+      username,
+      password: hashPassword(password)
+    });
+  }
+
+  return res.redirect("/");
+});
+
+passportRouter.get(
+  "/private-page",
+  ensureLogin.ensureLoggedIn(),
+  (req, res) => {
+    res.render("passport/private", { user: req.user });
+  }
+);
 
 module.exports = passportRouter;
