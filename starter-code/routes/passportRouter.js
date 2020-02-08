6d8f2656c04d3e903c092a540382650c083ad8c5
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-02-08 18:24:16 +0100

    Added singup info

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,11 +1,18 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
 // Require user model
-
+//const User = require("..models/user");
 // Add bcrypt to encrypt passwords
 
 // Add passport 
 
+/*Add a new route to your passportRouter.js file
+ with the path /signup and point it to your 
+ views/passport/signup.hbs file.
+*/
+passportRouter.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
+});
 
 const ensureLogin = require("connect-ensure-login");
 
@@ -13,5 +20,9 @@ const ensureLogin = require("connect-ensure-login");
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+
+
+
 
 module.exports = passportRouter;
