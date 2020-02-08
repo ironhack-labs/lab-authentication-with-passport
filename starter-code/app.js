3b9f23b882615e9a7460d6a0e244ec59909d65f3
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-02-08 21:50:37 +0100

    Add register form and basic layout, import connect-mongo, flash and passport

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,6 +8,9 @@ const hbs = require("hbs");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("connect-flash");
 
 mongoose
   .connect(process.env.DBURL, {
@@ -34,6 +37,16 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "Prehistoric Mamut",
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
+//require("./passport")(app);
+app.use(flash());
 
 // Express View engine setup
 
@@ -47,8 +60,15 @@ app.use(
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  res.locals.errors = req.flash("error");
+  next();
+});
 
 // default value for title local
 app.locals.title = "Express - Generated with IronGenerator";
@@ -56,8 +76,5 @@ app.locals.title = "Express - Generated with IronGenerator";
 // Routes middleware goes here
 const index = require("./routes/index");
 app.use("/", index);
-
-const passportRouter = require("./routes/passportRouter");
-app.use("/auth", passportRouter);
 
 module.exports = app;
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,19 +1,36 @@
 const express = require("express");
-const passportRouter = express.Router();
+const router = express.Router();
 // Require user model
-
+const User = require("../models/User");
 // Add bcrypt to encrypt passwords
-
+const { hashPassword, checkHashed } = require("../lib/hashing");
 // Add passport
-
+const passport = require("passport");
+// Add LoggedIn Middleware
 const ensureLogin = require("connect-ensure-login");
 
-passportRouter.get(
-  "/private-page",
-  ensureLogin.ensureLoggedIn(),
-  (req, res) => {
-    res.render("passport/private", { user: req.user });
-  }
-);
+router.get("/register", (req, res, next) => {
+  res.render("passport/register");
+});
 
-module.exports = passportRouter;
+router.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  const existingUser = await User.findOne({ username });
+  if (!existingUser) {
+    const newUser = User.create({
+      username,
+      password: hashPassword(password)
+    });
+    //req.flash(`Created user ${username}`)
+    return res.redirect("/");
+  } else {
+    req.flash("User already exist with this username");
+    return res.redirect("/auth/register");
+  }
+});
+
+router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("passport/private", { user: req.user });
+});
+
+module.exports = router;
