2d7bb5c7ddba302da85df1b2e498be0818bd98d7
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-02-09 13:55:17 +0100

    Add logged in middleware config

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -13,28 +13,33 @@ router.get("/register", ensureLogin.ensureLoggedOut(), (req, res, next) => {
   res.render("passport/register");
 });
 
-router.post("/register", async (req, res, next) => {
-  const { username, password } = req.body;
-  const existingUser = await User.findOne({ username });
-  if (!existingUser) {
-    const newUser = User.create({
-      username,
-      password: hashPassword(password)
-    });
-    //req.flash(`Created user ${username}`)
-    return res.redirect("/");
-  } else {
-    req.flash("User already exist with this username");
-    return res.redirect("/auth/register");
+router.post(
+  "/register",
+  ensureLogin.ensureLoggedOut(),
+  async (req, res, next) => {
+    const { username, password } = req.body;
+    const existingUser = await User.findOne({ username });
+    if (!existingUser) {
+      const newUser = User.create({
+        username,
+        password: hashPassword(password)
+      });
+      //req.flash(`Created user ${username}`)
+      return res.redirect("/");
+    } else {
+      req.flash("User already exist with this username");
+      return res.redirect("/auth/register");
+    }
   }
-});
+);
 
-router.get("/login", (req, res, next) => {
+router.get("/login", ensureLogin.ensureLoggedOut(), (req, res, next) => {
   res.render("passport/login");
 });
 
 router.post(
   "/login",
+  ensureLogin.ensureLoggedOut(),
   passport.authenticate("local", { failureRedirect: "/auth/login" }),
   function(req, res) {
     res.redirect("/");
@@ -49,10 +54,14 @@ app.post(
   }
 );*/
 
-router.get("/logout", async (req, res, next) => {
-  req.logout();
-  res.redirect("/");
-});
+router.get(
+  "/logout",
+  ensureLogin.ensureLoggedIn("/auth/login"),
+  async (req, res, next) => {
+    req.logout();
+    res.redirect("/");
+  }
+);
 
 router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
