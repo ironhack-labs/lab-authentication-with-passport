8bcc3572c08a660b18d0666c980f0d66d3969725
Ana Segarra <anasegarraber@gmail.com>
2020-02-09 01:44:33 +0100

    Style views and create form partial

--- starter-code/app.js
+++ starter-code/app.js
@@ -62,12 +62,14 @@ app.use(
 	require('node-sass-middleware')({
 		src: path.join(__dirname, 'public'),
 		dest: path.join(__dirname, 'public'),
-		sourceMap: true
+		sourceMap: true,
+		outputStyle: 'compressed'
 	})
 );
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(`${__dirname}/views/partials`);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
