d2cd745950fef07be19a5a13e6786ab649e7f492
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-02-09 20:05:35 +0100

    Update styles

--- starter-code/app.js
+++ starter-code/app.js
@@ -74,7 +74,7 @@ app.use((req, res, next) => {
 });
 
 // default value for title local
-app.locals.title = "Express - Generated with IronGenerator";
+app.locals.title = "Passport authentication";
 
 // Routes middleware goes here
 const index = require("./routes/index");
