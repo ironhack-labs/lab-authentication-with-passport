a73649e1bccc06ae7761f68f80512b37dd17ba86
Gonzalo lledo <gonzalo_lledo@outlook.com>
2020-02-10 16:34:30 +0100

    ha estado muy entretenido :)

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router  = express.Router();
 
+// Comprobar si un usuario tiene la sesión inciada
+const checkLoggedIn = (req, res, next) => req.user ? next() : res.render('index', { loginErrorMessage: 'Zona restringida a usuarios registrados' })
+
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
+router.get('/', (req, res, next) => res.render('index'))
+router.get("/private", checkLoggedIn, (req, res) => res.render("passport/private", { user: req.user }));
 
 module.exports = router;
