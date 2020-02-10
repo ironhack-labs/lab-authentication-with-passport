8c1061a4ae1ac710023e15aa1ac1ec2b4f65d145
Laura <lauratp02@gmail.com>
2020-02-10 16:47:11 +0100

    Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,26 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require("express-session");
+const User = require('./models/user');
 
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+// const ensureLogin = require("connect-ensure-login");
+const flash = require("connect-flash");
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/passport-login', {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -27,24 +36,86 @@ const app = express();
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+// app.use(require('node-sass-middleware')({
+//   src:  path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   sourceMap: true
+// }));
+
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+app.use(
+  session({
+    secret: "our-passport-local-strategy-app",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(flash());
 
+passport.use(
+  new LocalStrategy({
+      passReqToCallback: true
+    },
+    (req, username, password, next) => {
+      User.findOne({
+          username
+        },
+        (err, user) => {
+          // todo: watch with mongodb stopped
+          if (err) {
+            return next(err);
+          }
+          if (!user) {
+            return next(null, false, {
+              message: "Incorrect username"
+            });
+          }
+          if (!bcrypt.compareSync(password, user.password)) {
+            return next(null, false, {
+              message: "Incorrect password"
+            });
+          }
+          return next(null, user);
+        }
+      );
+    }
+  )
+);
+
+passport.serializeUser((user, cb) => {
+  console.log("serialize");
+  console.log(`storing ${user._id} in the session`);
+  cb(null, user._id);
+  // cb(null, {id: user._id, role: user.role});
+});
+
+
+passport.deserializeUser((id, cb) => {
+  console.log("deserialize");
+  console.log(`Attaching ${id} to req.user`);
+  // eslint-disable-next-line consistent-return
+  User.findById(id, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+app.use(passport.initialize());
+app.use(passport.session());
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,105 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
-// Require user model
+const User = require('../models/user');
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
+// const LocalStrategy = require("passport-local").Strategy;
+// const flash = require("connect-flash");
+const bcryptSalt = 10;
+
+
+// Require user modelco
+
 
 // Add bcrypt to encrypt passwords
+
 
 // Add passport 
 
 
-const ensureLogin = require("connect-ensure-login");
+
 
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+  res.render("passport/private", {
+    user: req.user
+  });
 });
+
+passportRouter.get('/signup', (req, res, next) => {
+  res.render('passport/signup')
+
+});
+
+passportRouter.post("/signup", (req, res, next) => {
+  console.log("hola")
+  const {
+    username,
+    password
+  } = req.body;
+  if (username === "" || password === "") {
+    res.render("passport/signup", {
+      message: "Indicate username and password",
+      section: "signup"
+    });
+    return;
+  }
+  User.findOne({
+      username
+    })
+    .then(user => {
+      if (user !== null) {
+        res.render("passport/signup", {
+          message: "The username already exists",
+          section: "signup"
+        });
+        return;
+      }
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+      const newUser = new User({
+        username,
+        password: hashPass
+      });
+      newUser.save(err => {
+        if (err) {
+          res.render("passport/signup", {
+            message: "Something went wrong",
+            section: "signup"
+          });
+        } else {
+          res.redirect("/login");
+          // res.render.json('hola')
+        }
+      });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+passportRouter.get("/login", (req, res) => {
+  res.render("passport/login", {
+    message: "error",
+    section: "login"
+  });
+});
+// invoked via passport.use(new LocalStrategy({
+passportRouter.post(
+  "/login",
+  passport.authenticate("local", {
+    successReturnToOrRedirect: "/private-page",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
+
+
+
+
+
+
+
 
 module.exports = passportRouter;
