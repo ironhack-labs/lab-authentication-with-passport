92e44ff3260b63fed054d236ac05762f561a4d2c
Fran <fmjerez@icloud.com>
2020-02-10 10:02:58 +0100

    appdone and withDbConnections done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,43 +1,98 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const flash = require("flash");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/user");
+const dbUrl = process.env.MONGODB_URL
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+
 
 const app = express();
 
-// Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
-// Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+app.use(session({
+  secret: "my-cat-has-three-legs",
+  resave: true,
+  saveUninitialized: true,
 }));
-      
+
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, callback) => {
+  User.findById(id)
+    .then(user => {
+      callback(null, user);
+    })
+    .catch(error => {
+      callback(error);
+    });
+});
+
+app.use(flash());
+
+passport.use(
+  new LocalStrategy({
+      passReqToCallback: true
+    },
+    (req, username, password, callback) => {
+      User.findOne({
+          username
+        })
+        .then(user => {
+          if (!user || !bcrypt.compareSync(password, user.password)) {
+            return callback(null, false, {
+              message: "Insert Username and Password again"
+            });
+          }
+          callback(null, user);
+        })
+        .catch(error => {
+          callback(error);
+        });
+    }
+  )
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -45,12 +100,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Authentication with passport';
 
 
-// Routes middleware goes here
 const index = require('./routes/index');
 app.use('/', index);
 const passportRouter = require("./routes/passportRouter");
