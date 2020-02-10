81bb930038f8035ddebfe3b30f1a03a4cc354888
Carlos Mateo <cmc2k5@gmail.com>
2020-02-10 15:04:12 +0100

    Sign up iteration

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,44 @@
-const express        = require("express");
+const express = require('express');
 const passportRouter = express.Router();
-// Require user model
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
 
-// Add bcrypt to encrypt passwords
+const ensureLogin = require('connect-ensure-login');
 
-// Add passport 
+function hashPassword(password) {
+    const salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password, salt);
+}
 
+passportRouter.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render('passport/private', { user: req.user });
+});
 
-const ensureLogin = require("connect-ensure-login");
+passportRouter.get("/signup", (req, res, next) =>
+    res.render("passport/signup")
+);
 
+passportRouter.post('/signup', async (req, res) => {
+    const {username, password} = req.body;
 
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+    if (!username || !password) {
+        req.flash('error', 'All fields are required!');
+        return res.redirect('/signup');
+    }
+
+    try {
+        const user = await User.findOne({ username });
+        if (user) {
+            req.flash('error', 'This user already exists. Please try again with a different Username.');
+            return res.redirect('/signup');
+        }
+        await User.create({ username, password: hashPassword(password) });
+        req.flash('sucess', 'User created successfully!');
+        return res.redirect('/')
+    } catch (e) {
+        next(e);
+    }
 });
 
 module.exports = passportRouter;
