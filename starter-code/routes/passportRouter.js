d58a8d45a084001efe20c77931b956e7a399d815
davidvrl <veral.david@gmail.com>
2020-02-10 16:50:33 +0100

    done

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,124 @@
-const express        = require("express");
-const passportRouter = express.Router();
+const express = require("express");
+const router = express.Router();
+
 // Require user model
+const User = require("../models/user");
 
 // Add bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
-// Add passport 
-
+// Add passport
+const passport = require("passport");
 
 const ensureLogin = require("connect-ensure-login");
 
+// SIGN UP
+router.get("/signup", (req, res) => res.render("passport/signup"));
 
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+router.post("/signup", (req, res) => {
+  const {
+    username,
+    password
+  } = req.body;
+
+  if (username === "" || password === "") {
+    res.render("passport/signup", {
+      message: "Rellena los campos"
+    });
+    return;
+  }
+
+  User.findOne({
+      username
+    })
+    .then(user => {
+      if (user) {
+        res.render("passport/signup", {
+          message: "El usuario ya existe"
+        });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      User.create({
+          username,
+          password: hashPass
+        })
+        .then(() => res.redirect("/"))
+        .catch(() =>
+          res.render("passport/signup", {
+            message: "Something went wrong"
+          })
+        );
+    })
+    .catch(error => next(error));
 });
 
-module.exports = passportRouter;
+//LOGIN
+router.get("/login", (req, res) => res.render("passport/login"));
+
+// router.get("/login", (req, res) =>
+//   res.render("passport/login", {
+
+
+//     message: req.flash("error")
+//   })
+// );
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
+
+router.get("/login", (req, res) => res.render("passport/login", {
+  message: req.flash("error")
+}))
+router.post('/login', passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}))
+
+// router.post("/login", (req, res) => {
+//   const {
+//     username,
+//     password
+//   } = req.body;
+
+//   if (username === "" || password === "") {
+//     res.render("passport/login", {
+//       message: "Rellena los campos"
+//     });
+//     return;
+//   }
+//   passport.authenticate("local", {
+//       successRedirect: "/",
+//       failureRedirect: "/login",
+//       failureFlash: true,
+//       passReqToCallback: true
+//     })
+//     .catch(error => next(error));
+// });
+
+
+//Logout
+router.get("/logout", (req, res) => {
+  req.logout()
+  res.redirect("/login")
+})
+
+router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("passport/private", {
+    user: req.user
+  });
+});
+
+module.exports = router;
