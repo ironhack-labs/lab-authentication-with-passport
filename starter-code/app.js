14dd4ea44c795473ec5665e117cb23d1ce565796
Fran <fmjerez@icloud.com>
2020-02-10 18:54:33 +0100

    finished

--- starter-code/app.js
+++ starter-code/app.js
@@ -35,6 +35,14 @@ const app_name = require("./package.json").name;
 
 const app = express();
 
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
