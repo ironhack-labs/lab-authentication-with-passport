7b7a21faf976e4636e5fd820d1765d5edde8e914
Mayank Srivastava <mayank.srivastav84@gmail.com>
2020-04-05 00:56:15 +0200

    Done

--- authenticate-with-passport/app.js
+++ authenticate-with-passport/app.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-const bcrypt       = require('bcrypt')
+const bodyParser    = require('body-parser');
+const cookieParser  = require('cookie-parser');
+const express       = require('express');
+const favicon       = require('serve-favicon');
+const hbs           = require('hbs');
+const mongoose      = require('mongoose');
+const logger        = require('morgan');
+const path          = require('path');
+const bcrypt        = require('bcrypt')
+const session       = require('express-session')
+const passport      = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User          = require('./models/user')
+const flash         = require('connect-flash');
+
 
 
 
@@ -34,12 +40,59 @@ app.use(cookieParser());
 
 // Express View engine setup
 
+app.use(flash());
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
       
+
+app.use(
+  session({
+    secret: 'our-passport-local-strategy-app',
+    resave: true,
+    saveUninitialized: true
+  })
+)
+
+passport.serializeUser((user, callback) => {
+  callback(null, user._id);
+});
+
+passport.deserializeUser((id, callback) => {
+  User.findById(id)
+    .then(user => {
+      callback(null, user);
+    })
+    .catch(error => {
+      callback(error);
+    });
+});
+
+passport.use(
+  new LocalStrategy({
+    passReqToCallback: true
+  }, (req,username, password, callback) => {
+    User.findOne({ username })
+      .then(user => {
+        if (!user) {
+          return callback(null, false, { message: 'Incorrect username' });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return callback(null, false, { message: 'Incorrect password' });
+        }
+        callback(null, user);
+      })
+      .catch(error => {
+        callback(error);
+      });
+  })
+);
+
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- authenticate-with-passport/routes/auth-routes.js
+++ authenticate-with-passport/routes/auth-routes.js
@@ -2,8 +2,27 @@ const express       = require ('express')
 const router        = express.Router();
 const User          = require('../models/user')
 const bcrypt        = require('bcrypt');
-const bcryptSalt    = 10
+const bcryptSalt    = 10;
+const passport      = require('passport')
+const ensureLogin   = require('connect-ensure-login');
+const flash         = require('connect-flash')
 
+
+//route for Home Page
+
+router.get('/', (req,res,next) =>{
+    res.render('index')
+})
+
+//route for logout
+
+router.get('/logout', (req,res) => {
+    req.logout();
+    res.redirect('/')
+})
+
+
+//Routes for Signup
 
 router.get('/signup', (req,res,next) =>{
     res.render('auth/signup')
@@ -48,7 +67,27 @@ router.post('/signup', (req,res,next) => {
 
 
 
+//Routes for Login
 
+router.get('/login', (req,res,next) => {
+    res.render('auth/login', {message: req.flash('error')})
+})
+
+router.post(
+    '/login', 
+    passport.authenticate('local', {
+        successRedirect: '/member',
+        failureRedirect: '/login', 
+        failureFlash: true,
+        passReqToCallback: true
+    })
+); 
+
+
+//routes for member 
+router.get('/member', ensureLogin.ensureLoggedIn(), (req,res) => {
+    res.render('auth/member', {user: req.user} )
+})
 
 
 module.exports = router;
