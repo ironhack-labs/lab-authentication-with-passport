33d2c4c3a254137e997f5277da3903a5593bf87b
Julia Foresti <juliajforesti@gmail.com>
2020-04-07 17:25:41 -0300

    done

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
+const session = require("express-session"); 
+const bcrypt = require("bcrypt"); 
+const passport = require("passport"); 
+const LocalStrategy = require("passport-local").Strategy;
+const User = require('./models/user');
 
 mongoose
   .connect('mongodb://localhost/auth-with-passport', {
@@ -28,6 +33,42 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ 
+  secret: "our-passport-local-strategy-app", 
+  resave: true, 
+  saveUninitialized: true,
+}));
+
+// user serialize and deserialize
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize()); 
+app.use(passport.session());
 
 // Express View engine setup
 
@@ -44,5 +85,6 @@ const index = require('./routes/index.routes');
 app.use('/', index);
 const authRoutes = require('./routes/auth.routes');
 app.use('/', authRoutes);
+
 
 module.exports = app;
