f721e1e1e12c40f6088c006ea5126551f035c1db
Manuel <mbuergel@gmx.de>
2020-04-18 17:09:45 +0200

    done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,58 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
+
 // Require user model
+const User = require("../models/user.model");
 
 // Add bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
 // Add passport
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
 
-const ensureLogin = require('connect-ensure-login');
-
-router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
+// private pages
+router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("auth/private", { user: req.user });
 });
+
+// signup
+router.get("/signup", (req, res) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", (req, res) => {
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(req.body.password, salt);
+
+  let user = new User({ username: req.body.username, password: hashPass });
+
+  user.save().then(() => {
+    res.redirect("/login");
+  });
+});
+
+// login
+router.get("/login", (req, res, next) => {
+  res.render("auth/login",  { message: req.flash('error') });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true,
+  })
+);
+
+// logout
+router.get("/logout", (req, res) => {
+  req.logout()
+  res.redirect("/")
+})
 
 module.exports = router;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const passport = require("passport");
 
 /* GET home page */
-router.get('/', (req, res) => res.render('index'));
+router.get('/', (req, res) => {
+    res.render('index', {user: req.user})
+});
 
 module.exports = router;
