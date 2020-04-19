92598fb59d11362bf8fb39eddcf33fd8ff86c561
Loriel Sher <loriel099@gmail.com>
2020-04-19 09:42:45 +0200

    done

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const MongoStore = require("connect-mongo")(session);
+
+const User = require("./models/User.model")
+
 mongoose
   .connect('mongodb://localhost/auth-with-passport', {
     useNewUrlParser: true,
@@ -22,6 +31,58 @@ const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+// express-session configuration 
+app.use(session({
+  secret: "abc",
+  cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 1 day
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    resave: true,
+    saveUninitialized: false,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+// associate user with a session // store the user into the session
+passport.serializeUser((user, callback) => {
+  callback(null, user._id);
+});
+
+// this happens on every single request (if the user is logged in // if user._id exists in the session)
+// it makes the current user available as req.user
+passport.deserializeUser((id, callback) => {
+  User.findById(id)
+    .then(user => {
+      callback(null, user);
+    })
+    .catch(error => {
+      callback(error);
+    });
+});
+
+passport.use(
+  new LocalStrategy((username, password, callback) => {
+    User.findOne({ username })
+      .then(user => {
+        if (!user) {
+          return callback(null, false, { message: 'Incorrect username' });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return callback(null, false, { message: 'Incorrect password' });
+        }
+        callback(null, user);
+      })
+      .catch(error => {
+        callback(error);
+      });
+  })
+);
+
+
+// basic passport setup
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -44,5 +105,15 @@ const index = require('./routes/index.routes');
 app.use('/', index);
 const authRoutes = require('./routes/auth.routes');
 app.use('/', authRoutes);
+
+
+// Configure session
+app.use(
+  session({
+    secret: 'our-passport-local-strategy-app',
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 module.exports = app;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const app = express()
+const passport = require('passport')
+const User = require('./../models/User.model')
+
+
+// signup form
+//why is it router.get and not app.get
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup')
+})
+
+router.post('/signup', (req, res, next) => {
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(req.body.password, salt)
+
+  let user = new User({
+    username: req.body.username,
+    password: hashPass
+  })
+  user.save().then(() => {
+    res.send('worked')
+  })
+
+})
+
 // Require user model
+// This is a syntax of how to import a database(a model) which is the User.model
+// const User = require('../models/User.model')
 
 // Add bcrypt to encrypt passwords
+//bcrypt is a library
+const bcrypt = require('bcrypt')
+const bcryptSalt = 10;
 
 // Add passport
 
 const ensureLogin = require('connect-ensure-login');
 
 router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
+  res.render('auth/private', { user: req.user });
 });
+
+// login form
+router.get('/login', (req, res, next) => {
+  res.render('../views/auth/login.hbs')
+})
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/private-page',
+  failureRedirect: '/login',
+  // failureFlash: true,
+  passReqToCallback: true
+}))
 
 module.exports = router;
