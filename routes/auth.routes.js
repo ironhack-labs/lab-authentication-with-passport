e5737f26f73aede6ad5b7ed96c3baa375b85bf16
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-20 15:16:58 +0200

    create signup form

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,58 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+
 // Require user model
+
+const User = require('../models/User.model')
 
 // Add bcrypt to encrypt passwords
 
+const bcrypt = require('bcryptjs')
+const bcryptSalt = 10
+
 // Add passport
 
-const ensureLogin = require('connect-ensure-login');
+router.get('/signup', (req, res) => {
+  res.render('auth/signup')
+})
+
+router.post('/signup', (req, res) => {
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    res.render('auth/signup', {
+      errorMsg: 'Por favor, rellena el formulario con usuario y contraseña.',
+    })
+    return
+  }
+
+  User.findOne({ username })
+    .then((user) => {
+      if (user) {
+        res.render('auth/signup', { errorMsg: 'Este usuario ya existe' })
+        return
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt)
+      const hashPass = bcrypt.hashSync(password, salt)
+
+      User.create({ username, password: hashPass })
+        .then(() => {
+          res.redirect('/')
+        })
+        .catch((err) => {
+          res.render('auth/signup', {
+            errorMsg: 'No se ha podido crear el usuario',
+          })
+        })
+    })
+    .catch((err) => next(err))
+})
+
+const ensureLogin = require('connect-ensure-login')
 
 router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
-});
+  res.render('passport/private', { user: req.user })
+})
 
-module.exports = router;
+module.exports = router
