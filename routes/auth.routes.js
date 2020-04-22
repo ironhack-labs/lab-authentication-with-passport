23cbe1802fab967c51351557c1b205509166bf7b
Karl Secen <karl.secen@gmail.com>
2020-04-22 21:25:13 +0200

    iteration 1

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,53 @@
 const express = require('express');
 const router = express.Router();
 // Require user model
+const User = require('../models/User.model');
 
 // Add bcrypt to encrypt passwords
-
+const bcryptjs = require('bcryptjs');
 // Add passport
+const passport = require('passport');
 
 const ensureLogin = require('connect-ensure-login');
 
 router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render('passport/private', { user: req.user });
+});
+
+router.get('/signup', (req, res) => {
+  res.render('auth/signup')
+});
+
+router.post('/signup', (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (password.length < 8) {
+    res.render('auth/signup', {
+      message: 'Your password must be 8 characters minimun.'
+    });
+    return;
+  }
+  if (username === '') {
+    res.render('auth/signup', { message: 'Your username cannot be empty' });
+    return;
+  }
+
+  User.findOne({ username: username }).then(found => {
+    if (found !== null) {
+      res.render('auth/signup', { message: 'This username is already taken' });
+    } else {
+      const salt = bcryptjs.genSaltSync();
+      const hash = bcryptjs.hashSync(password, salt);
+
+      User.create({ username: username, password: hash })
+        .then(dbUser => {
+              res.redirect('/');
+        })
+        .catch(err => {
+          next(err);
+        });
+    }
+  });
 });
 
 module.exports = router;
