4a641974f56a2eeeeaf469d7e7af58ef4f72268f
Jonathan Stein <jony.stn@gmail.com>
2020-04-23 11:36:27 +0200

    not finished

--- app.js
+++ app.js
@@ -8,18 +8,29 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
+const User = require('./models/User.model');
+const flash = require('connect-flash');
+const bcrypt = require('bcrypt');
+
+// require passport and the local strategy
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 mongoose
   .connect('mongodb://localhost/auth-with-passport', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error('Error connecting to mongo', err));
 
 const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const debug = require('debug')(
+  `${app_name}:${path.basename(__filename).split('.')[0]}`
+);
 
 const app = express();
 
@@ -28,6 +39,65 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Iteration #2: configure the session
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    saveUninitialized: false,
+    resave: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 * 1000,
+    }),
+  })
+);
+
+//Iteration #2: we serialize only the `_id` field of the user to keep the information stored minimum
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+//Iteration #2: when we need the information for the user, the deserializeUser function is called
+// with the id that we previously serialized to fetch the user from the database
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((dbUser) => {
+      done(null, dbUser);
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
+
+app.use(flash());
+
+//Iteration #2: LocalStrategy it finds the user and check if the password matches.
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username })
+      .then((found) => {
+        if (found === null) {
+          done(null, false, { message: 'Wrong credentials' });
+        } else if (!bcrypt.compareSync(password, found.password)) {
+          done(null, false, { message: 'Wrong credentials' });
+        } else {
+          done(null, found);
+        }
+      })
+      .catch((err) => {
+        done(err, false);
+      });
+  })
+);
+
+//Iteration #2: register passport as middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Express View engine setup
 
@@ -42,7 +112,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 // Routes middleware goes here
 const index = require('./routes/index.routes');
 app.use('/', index);
+
 const authRoutes = require('./routes/auth.routes');
-app.use('/', authRoutes);
+app.use('/auth', authRoutes);
 
 module.exports = app;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,76 @@
 const express = require('express');
 const router = express.Router();
-// Require user model
+//Iteration #1: Require user model
+const User = require('../models/User.model');
 
 // Add bcrypt to encrypt passwords
-
-// Add passport
+const bcrypt = require('bcrypt');
+//Iteration #2:  Add passport
+const passport = require('passport');
+// require passport and the local strategy. Do I need it here or in app.js?
+// const LocalStrategy = require('passport-local').Strategy;
 
 const ensureLogin = require('connect-ensure-login');
 
+// Iteration #1: The signup feature
+router.get('/signup', (req, res) => {
+  res.render('auth/signup');
+});
+
+//Iteration #2: This route needs to receive the data from the form and log the user in.
+router.post('/signup', (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (password.length < 8) {
+    res.render('auth/signup', {
+      message: 'Your password must be 8 characters minimun.',
+    });
+    return;
+  }
+  if (username === '') {
+    res.render('auth/signup', { message: 'Your username cannot be empty' });
+    return;
+  }
+  User.findOne({ username: username }).then((user) => {
+    if (user != null) {
+      res.render('auth/signup', { message: 'Your username alredy exists' });
+      return;
+    }
+    const salt = bcrypt.genSaltSync();
+    const hash = bcrypt.hashSync(password, salt);
+
+    User.create({ username: username, password: hash })
+      .then((newUser) => {
+        req.login(newUser, (err) => {
+          if (err) {
+            next(err);
+          } else {
+            res.redirect('/');
+          }
+        });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  });
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('auth/login', { errorMessage: req.flash('error') });
+});
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/private-page',
+    failureRedirect: '/login',
+    failureFlash: true,
+    passReqToCallback: true,
+  })
+);
+
 router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
+  res.render('auth/private', { user: req.user });
 });
 
 module.exports = router;
