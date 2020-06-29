4c4d1bf737be1dded3b13a7a1952d9b7b6d03a0e
Rodrigo Perez <user@Rodrigos-MacBook-Pro.local>
2020-06-29 17:04:36 +0200

    done

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
+
+require('./configs/passport.configs')(app)
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,57 @@
 const express = require('express');
 const router = express.Router();
 // Require user model
-
+const passport = require('passport')
+const User = require('../models/User.model')
 // Add bcrypt to encrypt passwords
+const bcrypt = require('bcrypt')
+
+const bcryptSalt = 10
 
 // Add passport
 
-const ensureLogin = require('connect-ensure-login');
+const ensureLogin = require('connect-ensure-login')
 
 router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
-});
+  res.render('auth/private', { user: req.user })
+})
 
+//--LOG IN--
+router.get('/signup', (req, res, next) => res.render('auth/signup'))
+router.post('/signup', (req, res, next) => {
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    res.render('auth/signup', {errorMsg:'Dame usuario y contrasena cuyons'})
+  }
+
+User.findOne({ username })
+  .then(user => {
+    if (user) {
+      res.render('auth/signup', {errorMsg: 'Eror en la BS' })
+      return 
+    }
+    const salt = bcrypt.genSaltSync(bcryptSalt)
+    const hashPass = bcrypt.hashSync(password, salt)
+    User.create({ username, password: hashPass })
+    .then(() => res.redirect('/'))
+    .catch(() => res.render('auth/signup', {errorMsg: 'No se pudo crear el usuario'}))
+  })
+  .catch(error => next(error))
+})
+
+router.get("/login", (req, res, next) => res.render("auth/login"))
+
+
+router.post("/login", passport.authenticate("local", {
+
+  successRedirect: "/",
+
+  failureRedirect: "/login",
+
+  failureFlash: true,
+
+  passReqToCallback: true
+
+}))
 module.exports = router;
