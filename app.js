c57834c4ff29ed14b3e82eff6821dcd56115e27a
Gabriel Moreno <gabidam1920@gmail.com>
2020-06-29 17:00:21 +0200

    done

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ mongoose
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
 const app = express();
 
 // Middleware Setup
@@ -28,6 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+require('./configs/passport.config')(app)
 
 // Express View engine setup
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,48 @@
 const express = require('express');
 const router = express.Router();
-// Require user model
+const User = require('../models/User.model')
+const bcrypt = require('bcrypt')
+const bcryptSalt = 10
+const passport = require('passport')
 
-// Add bcrypt to encrypt passwords
+const checkAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.redirect('/login')
 
-// Add passport
+router.get('/signup',(req,res) => res.render('auth/signup'))
 
-const ensureLogin = require('connect-ensure-login');
+router.post('/signup',(req,res) => {
+const {username,password} =req.body
 
-router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
-});
+if (username.length === 0 || password.length === 0) {
+  res.render("auth/signup", { errorMsg: "Fill the fields" });
+  return
+}
+
+User
+.findOne({ username })
+.then(user => {
+    if (user) {
+        res.render("auth/signup", { errorMsg: "The username already exists" });
+        return
+    }
+    const salt = bcrypt.genSaltSync(bcryptSalt)
+    const hashPass = bcrypt.hashSync(password, salt)
+
+    return User.create({ username, password: hashPass })
+})
+.then(userCreated => userCreated ? res.redirect('/') : null)
+.catch(err => console.log("Error!:", err))
+
+})
+
+router.get('/login',(req,res) => res.render('auth/login',{"message":req.flash('error')}))
+
+  router.post('/login', passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+}))
+
+router.get('/private-page', checkAuthenticated, (req, res) => res.render('auth/private', { user: req.user }))
 
 module.exports = router;
