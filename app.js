1252e3e2949c4dc4dd22ef5aa0255759aaf61009
Alba Mulero Otón <albamulero@MacBook-Pro-de-Alba.local>
2020-08-31 21:46:44 -0500

    User

--- app.js
+++ app.js
@@ -8,20 +8,32 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
+// Routes passport
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 
 mongoose
-  .connect('mongodb://localhost/auth-with-passport', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+    .connect('mongodb://localhost/auth-with-passport', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+    .catch(err => console.error('Error connecting to mongo', err));
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+// Inicializamos el passport
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -45,4 +57,57 @@ app.use('/', index);
 const authRoutes = require('./routes/auth.routes');
 app.use('/', authRoutes);
 
-module.exports = app;
+
+// Configuramos el express-session
+
+app.use(
+  session({
+    secret: "our-passport-local-strategy-app",
+    store: new MongoStore( { mongooseConnection: mongoose.connection } ),
+    resave: true,
+    saveUninitialized: true
+  })
+)
+
+
+
+const User = require('./models/User.model')
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id)
+})
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id)
+  .then(user => cb(null, user))
+  .catch(err => cb(err))
+})
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'username',
+    passportField: 'password'
+  },
+  (username, password, done) => {
+    User.findOne({ username })
+    
+    .then(user => {
+      
+      if (!user) {
+        return done(null, false, { message: 'Incorect username' })
+      }
+      
+      if (!bcrypt.compareSync(password, user.password)) {
+        return done(null, false, { message: 'Incorrect password'})
+      }
+
+      done(null, user)
+
+    })
+
+    .catch(err => done(err))
+
+  }
+  
+))
+
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,99 @@
 const express = require('express');
 const router = express.Router();
+
 // Require user model
+
+const User = require('../models/User.model')
 
 // Add bcrypt to encrypt passwords
 
+const bcrypt = require('bcrypt')
+const bcryptSalt = 12
+
 // Add passport
+
+const passport = require('passport')
 
 const ensureLogin = require('connect-ensure-login');
 
 router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render('passport/private', { user: req.user });
 });
+
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup')
+})
+
+router.post('/signup', (req, res, next) => {
+  const { username, password } = req.body
+
+  // Comprobamos que los campos no estan vacios
+
+  if (!username || !password) {
+    res.render('auth/signup', { errorMessage: 'Indicate username and password' })
+
+    return
+  }
+
+  User.findOne({ username })
+
+  .then((user) => {
+
+  // Comprobamos que el usuario no exista 
+
+  if (user = null) {
+    res.render('auth/signup', { Message: 'The username already exists' })
+
+      return
+  }
+
+  // Encriptamos la contraseña 
+
+  const salt = bcrypt.genSaltSync(bcryptSalt)
+  const hashPass = bcrypt.hashSync(password, salt)
+
+  // Creamos el nuevo usuario en la DB
+
+  const newUser = new User({
+    username,
+    password: hashPass
+  })
+
+  // El usuario se guarda en la DB
+  newUser
+  .save()                                            // Guardamos en DB
+  .then(() => res.redirect('/'))                    // Volvemos a la pag principal
+  .catch((err) => next(err))                       // En caso de err nos lo lanza
+
+  })
+
+  .catch((err) => next(err))
+
+})
+
+router.get('/login', (req, res, next) => {
+  res.render('auth/login')
+})
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}))
+
+// Pag de autentificacion
+
+router.get('/private-page', (req, res) => {
+  
+  if (!req.user) {
+    res.render('/login')
+    return
+  }
+
+  res.render('private', { user: req.user })
+})
+
+
+
+
 
 module.exports = router;
