d2bb44aa72d7418eeec786b9225065efb87f8504
Sergio <sergiobaltanasmontes@gmail.com>
2020-09-14 19:15:32 +0200

    done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,54 @@
 const express = require('express');
 const router = express.Router();
-// Require user model
+const passport = require('passport')
 
-// Add bcrypt to encrypt passwords
+const User = require('../models/user.model')
 
-// Add passport
+const bcrypt = require('bcrypt')
+const bcryptSalt = 10
 
-const ensureLogin = require('connect-ensure-login');
 
-router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
-});
+
+//Signup form
+router.get('/signup', (req, res) => res.render('auth/signup'))
+router.post('/signup', (req, res) => {
+
+  const { username, password } = req.body
+
+
+  if (username.length === 0 || password === 0) {
+    res.render('auth/signup', { message: 'Indicate username and password' })
+    return
+  }
+
+
+  User.findOne({ username })
+    .then(user => {
+      if (user) {
+        res.render('auth/signup', { message: 'The username already exists' })
+        return
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt)
+      const hashPass = bcrypt.hashSync(password, salt)
+
+
+      User.create({ username, password: hashPass })
+        .then(() => res.redirect('/'))
+        .catch(error => next(error))
+    })
+    .catch(error => next(error))
+})
+
+
+
+//Login
+router.get('/login', (req, res) => res.render('auth/login', { 'message': req.flash('error') }))
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}))
 
 module.exports = router;
