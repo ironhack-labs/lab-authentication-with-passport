2f36c17cc4f98460d4ec732b5539d32206fda5dc
Juan Pestana <juan.pestana82@gmail.com>
2020-09-14 19:57:49 +0200

    terminado

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,53 @@
-const express = require('express');
-const router = express.Router();
-// Require user model
+const express = require('express')
+const router = express.Router()
+const passport = require("passport")
 
-// Add bcrypt to encrypt passwords
+const User = require("../models/user.model")
 
-// Add passport
+const bcrypt = require("bcrypt")
+const bcryptSalt = 10
 
-const ensureLogin = require('connect-ensure-login');
+router.get('/signup', (req, res) => res.render('auth/signup'))
+router.post('/signup', (req, res, next) => {
 
-router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
-});
+    const {username, password} = req.body
 
-module.exports = router;
+    User.findOne({username})
+        .then(user =>{
+            if (user) {
+                res.render('auth/signup', {message : "El usuario ya existe"})
+                return
+            }
+
+            const salt = bcrypt.genSaltSync(bcryptSalt)
+            const hashPass = bcrypt.hashSync(password, salt)
+
+            User.create({username, password : hashPass})
+                .then(() => res.redirect('/login'))
+                .catch(error => next(error))
+        })
+        .catch(error => next(error))
+
+
+
+})
+
+router.get('/login', (req, res) => res.render('auth/login',{ "message": req.flash("error") }))
+router.post('/login', passport.authenticate("local", {
+    successRedirect: "/private-profile", //OJO DEFINIR RUTA PRIVADA 
+    failureRedirect: '/login',
+    failureFlash: true,
+    passReqToCallback: true
+}))
+
+
+const checkLoggedIn = (req, res, next) => req.isAuthenticated() ? next() : res.render('auth/login', { message: 'Desautorizado, incia sesión para continuar' })
+
+router.get('/private-profile', checkLoggedIn, (req, res) => res.render('profile', req.user))
+
+router.get('/logout', (req, res) =>{
+    req.logout()
+    res.render('auth/login', {message: 'has cerrado sesión'})
+})
+
+module.exports = router
