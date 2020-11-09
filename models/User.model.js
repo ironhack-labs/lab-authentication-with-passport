6c54c5443223e424de6aa7650e68d6bb7fb9580f
Luizzmy <luizzmy@example.com>
2020-11-09 17:28:02 -0600

    iteracion 1

--- models/User.model.js
+++ models/User.model.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
+const userSchema = new Schema(
+  {
+  username:{
+    type: String,
+    required: true,
+    unique:true
+    },
+  password:{
+    type: String,
+    required:true
+    } 
+  },
+  {
   timestamps: true
 });
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,11 @@
+const {genSaltSync}=require('bcrypt')
 const express = require('express');
 const router = express.Router();
 // Require user model
+const User=require('../models/User.model')
 
 // Add bcrypt to encrypt passwords
-
+const bcrypt=require('bcrypt')
 // Add passport
 
 const ensureLogin = require('connect-ensure-login');
@@ -12,4 +14,30 @@ router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render('passport/private', { user: req.user });
 });
 
+router.get('/signup',(req,res)=>{
+  res.render('auth/signup')
+})
+
+router.post('/signup', async (req,res)=>{
+  //1. recibir infor del form
+  const {username, password}=req.body
+  //2. verificar que no este vacio
+  if (!username || !password) {
+    return res.render('auth/signup', {error:"Missing fields"})
+  }else{
+  //3. verificar que no este en bd
+  const user=await User.findOne({username})
+  if(user){
+    return res.render('auth/signup', {error:"Please try with a different username"})
+  } 
+    //4. Guardar en bd con contraseña hasheada
+    const salt=bcrypt.genSaltSync(12)
+    const hashpwd=bcrypt.hashSync(password,salt)
+    await User.create({username,password:hashpwd})
+    //5. Dar respuesta al usuario
+    res.redirect('private')
+  }
+})
+
 module.exports = router;
+
