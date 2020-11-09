f203fb89c4506e633d7e6466d7ae74aceace7ca0
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-09 16:35:36 -0600

    user con bcrypt

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,9 +2,8 @@ const express = require('express'),
   router = express.Router(),
   chalkAnimation = require('chalk-animation'),
   app = express(),
-  crypt = require("bcrypt"),
+  bcrypt = require("bcrypt"),
   User = require("../models/User.model.js");
-;
 // Require user model
 
 // Add bcrypt to encrypt passwords
@@ -14,7 +13,9 @@ const express = require('express'),
 const ensureLogin = require('connect-ensure-login');
 //Add a new GET route to your routes/auth.routes.js file with the path /signup and point it to your views/auth/signup.hbs file.
 router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
+  res.render('passport/private', {
+    user: req.user
+  });
 });
 
 router.get('/signup', (req, res) => {
@@ -23,15 +24,22 @@ router.get('/signup', (req, res) => {
 // add a POST route to your routes/auth.routes.js to receive the data from the signup form and create a new user with the data.
 router.post('/signup', (req, res) => {
   // console.log(req.body);
-  const  { username, password} = req.body
-  let user = User.create(
-    {username: username,
-    password: password}
-  )
+  const {
+    username,
+    password
+  } = req.body
+  const salt = bcrypt.genSaltSync(12)
+  const hashPass = bcrypt.hashSync(password, salt)
+  let user = User.create({
+    username: username,
+    password: hashPass
+  })
 
 
 
-  res.render('auth/signup', { user});
+  res.render('auth/signup', {
+    user
+  });
   // console.log(user);
 });
 
@@ -41,9 +49,15 @@ router.get('/login', (req, res) => {
 });
 router.post('/login', (req, res) => {
   // console.log(req.body);
-  const  { username, password} = req.body
+  const {
+    username,
+    password
+  } = req.body
   console.log(username, password);
-  res.render('auth/login', { username, password});
+  res.render('auth/login', {
+    username,
+    password
+  });
   // console.log(user);
 });
 
