6c726966a9c7e5ce2a32a67d80b871549131c093
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-09 18:24:40 -0600

    no flash

--- config/passport.js
+++ config/passport.js
@@ -12,10 +12,10 @@ passport.use(
     async (username, password, done) => {
       const user = await User.findOne({ username })
       if (!user) {
-        return done(null, false, { message: "Incorrect username" })
+        return done(null, false)
       }
       if (!bcrypt.compareSync(password, user.password)) {
-        return done(null, false, { message: "Incorrect password" })
+        return done(null, false)
       }
 
       done(null, user) // envia el usuario a serializeUser
