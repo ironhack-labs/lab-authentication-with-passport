cac9bde15467c41eeb120a6c301ef8b5e75e6688
Isabel Perez <isabel.perez.ruiz25@gmail.com>
2020-11-16 16:20:13 +0100

    done

---
+++ routes/base.routes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const ensureAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.render('auth/login', { errorMsg: 'Desautorizado, inicia sesión' })
+// Endpoints
+router.get('/', (req, res) => res.render('index'))
+router.get('/private', ensureAuthenticated, (req, res) =>{ res.render('auth/private', { user: req.user});console.log(req.user)})
+
+module.exports = router
