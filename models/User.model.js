d0830d7e6f7ac8f6ca71b02adb2ad4178330e489
Cristy Lopez Piñeiro <aclopezp93@gmail.com>
2020-11-16 16:27:03 +0100

    done

--- models/User.model.js
+++ models/User.model.js
@@ -1,12 +1,14 @@
-const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+    username: String,
+    password: String,
+    
 }, {
-  timestamps: true
-});
+    timestamps: true
+})
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+const User = mongoose.model("User", userSchema)
+
+module.exports = User
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,78 @@
-const express = require('express');
-const router = express.Router();
-// Require user model
+const express = require('express')
+const router = express.Router()
+const passport = require("passport")
 
-// Add bcrypt to encrypt passwords
 
-// Add passport
+const User = require("../models/user.model")
 
-const ensureLogin = require('connect-ensure-login');
+const bcrypt = require("bcrypt")
+const bcryptSalt = 10
 
-router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
-});
+const ensureAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.render('auth/login', { errorMsg: 'Not autorized, sign in' })
 
-module.exports = router;
+
+
+// Registro (renderizado formualrio)
+router.get("/signup", (req, res) => res.render("auth/signup"))
+
+// Registro (gestión)
+router.post("/signup", (req, res, next) => {
+
+    const { username, password } = req.body
+
+    if (username === "" || password === "") {
+        res.render("auth/signup", { errorMsg: "Fill all fields" })
+        return
+    }
+
+    User
+        .findOne({ username })
+        .then(user => {
+            if (user) {
+                res.render("auth/signup", { errorMsg: "Username taken" })
+                return
+            }
+
+            // Other validations
+            const salt = bcrypt.genSaltSync(bcryptSalt)
+            const hashPass = bcrypt.hashSync(password, salt)
+
+            User.create({ username, password: hashPass })
+                .then(() => res.redirect('/'))
+                .catch(() => res.render("auth/signup", { errorMsg: "Error" }))
+        })
+        .catch(error => next(error))
+})
+
+
+
+// Inicio sesión (renderizado formulario)
+router.get("/login", (req, res) => res.render("auth/login", { errorMsg: req.flash("error") }))
+
+// Inicio sesión (gestión)
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+}))
+
+
+
+
+// Private page
+router.get('/private-page', ensureAuthenticated, (req, res) => res.render('auth/private', { user: req.user }))
+
+
+
+
+
+// Cerrar sesión
+router.get('/signout', (req, res) => {
+    req.logout()
+    res.redirect("/login")
+})
+
+
+
+module.exports = router
