66f6454546973cb83c05bb19f94c8f4a2e1707bb
FunkeFelix <felixfunke456@gmail.com>
2020-11-20 09:03:21 +0100

    bonus lab for passport now also incl. social login aswell as local authen

--- app.js
+++ app.js
@@ -1,48 +1,144 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
 mongoose
-  .connect('mongodb://localhost/auth-with-passport', {
+  .connect("mongodb://localhost/auth-with-passport", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+//passport config
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    saveUninitialized: false,
+    resave: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 * 1000,
+    }),
+  })
+);
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/User.model");
+const bcrypt = require("bcrypt");
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((dbUser) => {
+      done(null, dbUser);
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username })
+      .then((found) => {
+        if (found === null) {
+          done(null, false, { message: "Wrong credentials" });
+        } else if (!bcrypt.compareSync(password, found.password)) {
+          done(null, false, { message: "Wrong credentials" });
+        } else {
+          done(null, found);
+        }
+      })
+      .catch((err) => {
+        done(err, false);
+      });
+  })
+);
+
+const GithubStrategy = require("passport-github").Strategy;
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      callbackURL: "http://localhost:3000/github/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // find a user with profile.id as githubId or create one
+      console.log(profile);
+      User.findOne({ githubId: profile.id })
+        .then((found) => {
+          if (found !== null) {
+            // user already exists
+            done(null, found);
+          } else {
+            // no user with that github id
+            console.log(profile);
+            return User.create({
+              githubId: profile.id,
+              name: profile._json.login,
+              avatar: profile._json.avatar_url,
+            }).then((dbUser) => {
+              done(null, dbUser);
+            });
+          }
+        })
+        .catch((error) => {
+          done(error);
+        });
+    }
+  )
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Express View engine setup
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Express - Generated with IronGenerator";
 
 // Routes middleware goes here
-const index = require('./routes/index.routes');
-app.use('/', index);
-const authRoutes = require('./routes/auth.routes');
-app.use('/', authRoutes);
+const index = require("./routes/index.routes");
+app.use("/", index);
+const authRoutes = require("./routes/auth.routes");
+app.use("/", authRoutes);
 
 module.exports = app;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,78 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 // Require user model
+const User = require("../models/User.model");
 
 // Add bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
 
 // Add passport
+const passport = require("passport");
 
-const ensureLogin = require('connect-ensure-login');
+const ensureLogin = require("connect-ensure-login");
 
-router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+router.get("/github", passport.authenticate("github"));
+
+router.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    successRedirect: "/privat",
+    failureRedirect: "/login",
+  })
+);
+
+router.post("/signup", (req, res, next) => {
+  const { username, password } = req.body;
+  if (password.length < 8) {
+    res.render("auth/signup", {
+      message: "Your password must include more then 8 Characters!",
+    });
+  }
+  if (username === "") {
+    res.render("auth/signup", { message: "Please enter a Username" });
+  }
+  User.findOne({ username: username }).then((found) => {
+    if (found !== null) {
+      res.render("auth/signup", { message: "Username is already taken!" });
+    } else {
+      const salt = bcrypt.genSaltSync();
+      const hash = bcrypt.hashSync(password, salt);
+      User.create({ username: username, password: hash }).then((newUser) => {
+        req
+          .login(newUser, (err) => {
+            if (err) {
+              next(err);
+            } else {
+              res.redirect("/privat");
+            }
+          })
+          .catch((err) => {
+            next(err);
+          });
+      });
+    }
+  });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/privat",
+    failureRedirect: "/login",
+    passReqToCallback: true,
+  })
+);
+
+router.get("/privat", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("passport/privat", { user: req.user });
 });
 
 module.exports = router;
