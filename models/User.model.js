0716b359ffac2da73e274cbb8ca839c024d7b22a
Alberto Guzman <albertogx@icloud.com>
2020-11-29 22:39:47 -0500

    commit

--- models/User.model.js
+++ models/User.model.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: true
-});
+// const UserSchema = new mongoose.Schema({ });
+const userSchema = new Schema(
+  {
+    username: String,
+    password: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
