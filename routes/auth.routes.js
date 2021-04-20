3874448da2a3be7047849992efea172fbaa4fa92
Sisco Falcó <siscofalcop@gmail.com>
2021-04-20 15:15:00 +0200

    Iteration 1 done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+const User = require('../models/User.model')
 // Require user model
 
 // Add bcrypt to encrypt passwords
@@ -9,7 +12,45 @@ const router = express.Router();
 const ensureLogin = require('connect-ensure-login');
 
 router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
+  res.render('passport/private', {
+    user: req.user
+  });
 });
+
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup')
+})
+
+router.post('/signup', (req, res, next) => {
+  const {
+    username,
+    password
+  } = req.body;
+  User.findOne({
+      username
+    })
+    .then(user => {
+      if (user) {
+        res.render('auth/signup', {
+          errorMessage: 'User already exists'
+        });
+      }
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashPass = bcrypt.hashSync(password, salt);
+      User.create({
+          username,
+          password: hashPass
+        })
+        .then(newUser => {
+          return res.redirect('/')
+          // req.login(newUser, (error) => {
+          //   if (error) {
+          //     next(error)
+          //   }
+          //   return res.redirect('/passport/private')
+          // })
+        })
+    })
+})
 
 module.exports = router;
